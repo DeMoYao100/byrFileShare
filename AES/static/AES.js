b380ce98810fdab49b581cd821cb45d539ad23a6
moyaoxue <1823821430@qq.com>
2023-09-01 20:49:11 +0800

    Add files via upload

--- AES/static/AES.js
+++ AES/static/AES.js
@@ -10,20 +10,32 @@ async function generateKey() {
     return key;
   }
   
-  async function encryptArrayBuffer(key, arrayBuffer) {
-    const iv = crypto.getRandomValues(new Uint8Array(16));
-    const ciphertext = await crypto.subtle.encrypt(
-      {
-        name: "AES-CBC",
-        iv: iv
-      },
-      key,
-      arrayBuffer
-    );
+  // async function encryptArrayBuffer(key, arrayBuffer) {
+  //   const iv = crypto.getRandomValues(new Uint8Array(16));
+  //   const ciphertext = await crypto.subtle.encrypt(
+  //     {
+  //       name: "AES-CBC",
+  //       iv: iv
+  //     },
+  //     key,
+  //     arrayBuffer
+  //   );
   
-    return { ciphertext, iv };
-  }
-  
+  //   return { ciphertext, iv };
+  // }
+
+async function encryptArrayBuffer(key, arrayBuffer) {
+  const ciphertext = await crypto.subtle.encrypt(
+    {
+      name: "AES-CBC",
+      iv: fixedIv
+    },
+    key,
+    arrayBuffer
+  );
+  return { ciphertext, iv: fixedIv };
+}
+
   async function encryptAndUpload() {
     const fileInput = document.getElementById("fileInput");
     const file = fileInput.files[0];
@@ -67,10 +79,47 @@ async function generateKey() {
         });
     };
   
-    reader.onerror = function (event) {
-      console.error("File read error:", event.target.error);
-    };
-  
     reader.readAsArrayBuffer(file);
   }
-  
+
+  async function decryptAndDownload() {
+    const fixedIv = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]); // 16 bytes
+    const response = await fetch('/download/test.txt.enc', { method: 'GET' });
+    const blob = await response.blob();
+    const reader = new FileReader();
+    reader.onload = async function(event) {
+      const arrayBuffer = event.target.result;
+
+      const ivReader = new FileReader();
+      
+      ivReader.onload = async function(eventIv) {
+        const rawKey = new TextEncoder().encode('This_is_an_example_key_for_demo!');
+        const key = await crypto.subtle.importKey(
+          'raw', 
+          rawKey, 
+          { name: 'AES-CBC', length: 256 }, 
+          true, 
+          ['encrypt', 'decrypt']
+        );
+        
+        const decryptedData = await crypto.subtle.decrypt(
+          { name: "AES-CBC", iv: fixedIv },
+          key,
+          arrayBuffer
+        );
+        
+        // 下面的代码将解密的ArrayBuffer保存为文件
+        const blob = new Blob([new Uint8Array(decryptedData)]);
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = window.URL.createObjectURL(blob);
+        link.download = 'decrypted_file';
+        link.click();
+        window.URL.revokeObjectURL(url);
+      };
+      
+      ivReader.readAsArrayBuffer(blob);
+    };
+    
+    reader.readAsArrayBuffer(blob);
+  }
--- AES/test.py
+++ AES/test.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, send_from_directory
 import os
 
 from Crypto.Cipher import AES
@@ -10,6 +10,8 @@ app = Flask(__name__)
 def index():
     return render_template('./test.html')
 
+UPLOAD_FOLDER = 'E:\\大三上\\小学期\\加密\\AES\\download' #test
+
 @app.route('/',methods = ['POST'])
 def upload_file():
     if 'fileInput' not in request.files:
@@ -19,36 +21,43 @@ def upload_file():
     if file.filename == '':
         return jsonify({"error": "No selected file"}), 400
 
-    # 保存文件
     if file:
         filename = file.filename
         file_path = os.path.join('uploads', filename)
-
         if not os.path.exists('uploads'):
             os.makedirs('uploads')
 
         file.save(file_path)
 
 # 测试解密代码：
-    def decrypt_file(file_path, key):
-        with open(file_path, 'rb') as f:
-            iv_and_ciphertext = f.read()
+    # def decrypt_file(file_path, key):
+    #     with open(file_path, 'rb') as f:
+    #         iv_and_ciphertext = f.read()
 
-        iv = request.files['iv'].read()
-        ciphertext = iv_and_ciphertext
+    #     iv = request.files['iv'].read()
+    #     ciphertext = iv_and_ciphertext
 
-        cipher = AES.new(key, AES.MODE_CBC, iv)
+    #     cipher = AES.new(key, AES.MODE_CBC, iv)
 
-        plaintext_with_padding = cipher.decrypt(ciphertext)
+    #     plaintext_with_padding = cipher.decrypt(ciphertext)
 
-        padding_length = plaintext_with_padding[-1]
-        plaintext = plaintext_with_padding[:-padding_length]
+    #     padding_length = plaintext_with_padding[-1]
+    #     plaintext = plaintext_with_padding[:-padding_length]
 
-        with open(file_path[:-4] + ".dec", 'wb') as f:
-            f.write(plaintext)
+    #     with open(file_path[:-4] + ".dec", 'wb') as f:
+    #         f.write(plaintext)
 
-    decrypt_file(file_path,b'This_is_an_example_key_for_demo!')
+    # decrypt_file(file_path,b'This_is_an_example_key_for_demo!')
     return jsonify({"message": f"File {filename} has been uploaded successfully!"}), 200
+
+@app.route('/download',methods = ['GET'])
+def download_index():
+    return render_template('./download.html')
+
+
+@app.route('/download/<filename>',methods = ['GET'])
+def download_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
