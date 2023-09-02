7f65de939c51aed39f38c8c4bb9608814f0fd063
Windy <1816774520@qq.com>
2023-09-02 09:26:19 +0800

    update

--- frontend/frontend_readme.md
+++ frontend/frontend_readme.md
@@ -2,6 +2,7 @@
 
 ## 项目初始化
 ```
+cd frontend
 npm install
 ```
 ## 安装vue脚手架工具
@@ -12,8 +13,18 @@ npm install -g @vue/cli
 ```
 npm install --save-dev @arco-design/web-vue
 ```
+## 安装axios(一个调用后端API的工具)
+```
+npm install axios
+```
+
+## 安装代码生成工具：
+```
+npm install openapi-typescript-codegen --save-dev
+```
 ## 运行项目
 WebStorm中点击package.json
 
 点击第六行左边的红色箭头即可
 
+## 项目入口（理解程序运行）
--- frontend/src/store/user.ts
+++ frontend/src/store/user.ts
@@ -1,5 +1,7 @@
-import { StoreOptions } from "vuex";
+import axios from "axios";
 import ACCESS_ENUM from "@/access/accessEnum";
+import { StoreOptions } from "vuex";
+
 export default {
   namespaced: true,
   state: () => ({
@@ -9,8 +11,31 @@ export default {
     },
   }),
   actions: {
-    getLoginUser({ commit, state }, payload) {
-      commit("updateUser", payload);
+    async getLoginUser({ commit, state }) {
+      try {
+        const response = await axios.get(
+          "http://localhost:????/api/user/get/login"
+        );
+        const res = response.data;
+        if (res.code === 0) {
+          commit("updateUser", {
+            ...state.loginUser,
+            userRole: ACCESS_ENUM.USER,
+            // 其他你需要更新的字段
+          });
+        } else {
+          commit("updateUser", {
+            ...state.loginUser,
+            userRole: ACCESS_ENUM.NOT_LOGIN,
+          });
+        }
+      } catch (error) {
+        console.error("Error fetching login user:", error);
+        commit("updateUser", {
+          ...state.loginUser,
+          userRole: ACCESS_ENUM.NOT_LOGIN,
+        });
+      }
     },
   },
   mutations: {
