1e4b8581c08790a39dd4e8ff20578cbcaa2822fb
Jinyi Xia <jinyi.xia@bupt.edu.cn>
2023-09-02 21:46:22 +0800

    Update service.py

--- service/service.py
+++ service/service.py
@@ -1,7 +1,53 @@
 import db
 import file
+import os
 from typing import Optional
-from model import FileOpStatus
+from model import FileOpStatus, User
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+from datetime import datetime
+import secrets
+import hashlib
+
+
+def _send_mail(dest: str, authcode: str):
+    mail_html = f"""<p class=MsoNormal style='layout-grid-mode:char'><span style='font-size:14.0pt;
+font-family:宋体;mso-ascii-font-family:"Times New Roman";mso-hansi-font-family:
+"Times New Roman"'>您的验证码为：</span><span lang=EN-US style='font-size:14.0pt;
+font-family:"Times New Roman",serif;mso-fareast-font-family:宋体;mso-bidi-theme-font:
+minor-bidi'><o:p></o:p></span></p>
+<p class=MsoNormal align=center style='margin-top:7.8pt;margin-right:0cm;
+margin-bottom:7.8pt;margin-left:0cm;mso-para-margin-top:.5gd;mso-para-margin-right:
+0cm;mso-para-margin-bottom:.5gd;mso-para-margin-left:0cm;text-align:center;
+layout-grid-mode:char'><b><span lang=EN-US style='font-size:22.0pt;font-family:
+"Times New Roman",serif;mso-fareast-font-family:宋体;mso-bidi-font-family:Arial;
+letter-spacing:3.0pt'>{authcode}<o:p></o:p></span></b></p>
+<p class=MsoNormal style='layout-grid-mode:char'><span style='font-size:14.0pt;
+font-family:宋体;mso-ascii-font-family:"Times New Roman";mso-hansi-font-family:
+"Times New Roman"'>此验证码包含数字与大写英文字母，输入时请注意字母大小写是否正确。验证码</span><span lang=EN-US
+style='font-size:14.0pt;font-family:"Times New Roman",serif;mso-fareast-font-family:
+宋体;mso-bidi-theme-font:minor-bidi'>10</span><span style='font-size:14.0pt;
+font-family:宋体;mso-ascii-font-family:"Times New Roman";mso-hansi-font-family:
+"Times New Roman"'>分钟内有效。</span><span lang=EN-US style='font-size:14.0pt;
+font-family:"Times New Roman",serif;mso-fareast-font-family:宋体;mso-bidi-theme-font:
+minor-bidi'><o:p></o:p></span></p>"""
+    message = MIMEText(mail_html, 'html', 'utf-8')
+    message['From'] = 'Cloud Storage <jinyi.xia@foxmail.com>'
+    message['To'] = f'<{dest}>'
+    message['Subject'] = Header("验证码", 'utf-8')
+    message['Date'] = datetime.now().strftime('%a, %d %b %Y %H:%M:%S %z')
+    retval = True
+    try:
+        server = smtplib.SMTP_SSL('smtp.qq.com')
+        server.login('jinyi.xia@foxmail.com', 'owkqtmqtphhpdhhi')
+        server.sendmail('jinyi.xia@foxmail.com', dest, message.as_string())
+        # server.login('vericode_sender@yeah.net', 'ROHFXNVSDLWBIKFA')
+        # server.sendmail('vericode_sender@yeah.net', dest, message.as_string())
+        server.quit()
+    except:
+        retval = False
+    return retval
 
 
 def gen_authcode(email: str) -> bool:
@@ -13,6 +59,10 @@ def gen_authcode(email: str) -> bool:
     Returns:
         bool: True if the authcode was sent, False otherwise
     """
+    authcode = secrets.token_hex(4).upper()
+    if _send_mail(email, authcode):
+        db.update_authcode(email, authcode)
+        return True
     return False
 
 
@@ -26,7 +76,10 @@ def pwd_login_verify(email: str, pwd: str) -> bool:
     Returns:
         bool: True if verified, False otherwise
     """
-    return False
+    expected = db.get_user(email)
+    if expected is None:
+        return False
+    return hashlib.sha256(f'{pwd}{expected.salt}'.encode()).hexdigest() == expected.pwdhash
 
 
 def authcode_login_verify(email: str, authcode: str) -> bool:
@@ -39,7 +92,8 @@ def authcode_login_verify(email: str, authcode: str) -> bool:
     Returns:
         bool: True if verified, False otherwise
     """
-    return False
+    expected = db.get_authcode(email)
+    return expected == authcode
 
 
 def register(email: str, pwd: str, authcode: str) -> bool:
@@ -53,7 +107,14 @@ def register(email: str, pwd: str, authcode: str) -> bool:
     Returns:
         bool: True if registered, False otherwise
     """
-    return False
+    if db.get_user(email) is not None or authcode != db.get_authcode(email):
+        return False
+    salt = secrets.token_urlsafe(16)
+    pwdhash = hashlib.sha256(f'{pwd}{salt}'.encode()).hexdigest()
+    db.add_user(email, pwdhash, salt)
+    user_folder = hashlib.md5(email.encode()).hexdigest()
+    file.create_dir(user_folder)
+    return True
 
 
 def update_pwd(email: str, pwd: str, authcode: str) -> bool:
@@ -67,85 +128,110 @@ def update_pwd(email: str, pwd: str, authcode: str) -> bool:
     Returns:
         bool: True if updated, False otherwise
     """
-    return False
+    if db.get_user(email) is not None or authcode != db.get_authcode(email):
+        return False
+    salt = secrets.token_urlsafe(16)
+    pwdhash = hashlib.sha256(f'{pwd}{salt}'.encode()).hexdigest()
+    db.add_user(email, pwdhash, salt)
+    return True
 
 
-def get_dir_list(suffix: str, path: str) -> Optional[tuple[list[str], list[str]]]:
+def get_dir_list(prefix: str, path: str) -> Optional[tuple[list[str], list[str]]]:
     """Get the directory list of the user's path or group's path
 
     Args:
-        suffix (str): The email of the user or the id of the group
+        prefix (str): The email of the user or the id of the group
         path (str): The path of the directory
 
     Returns:
         tuple[list[str], list[str]]: A list of folders and a list of files, None if the path is invalid or the user/group does not exist
     """
-    return None
+    if '@' in prefix:
+        path_prefix = hashlib.md5(prefix.encode()).hexdigest()
+    else:
+        path_prefix = prefix
+    return file.get_dir_list(os.path.join(path_prefix, path))
 
 
-def get_file(suffix: str, full_name: str) -> Optional[bytes]:
+def get_file(prefix: str, full_name: str) -> Optional[bytes]:
     """Get the file list of the user's path or group's path
 
     Args:
-        suffix (str): The email of the user or the id of the group
+        prefix (str): The email of the user or the id of the group
         full_name (str): The path of the file (including file name)
 
     Returns:
         bytes: The file, None if the path is invalid or the user/group does not exist
     """
-    return None
+    if '@' in prefix:
+        path_prefix = hashlib.md5(prefix.encode()).hexdigest()
+    else:
+        path_prefix = prefix
+    return file.get_file(os.path.join(path_prefix, full_name))
 
 
-def put_file(suffix: str, full_name: str, content: bytes) -> FileOpStatus:
+def put_file(prefix: str, full_name: str, content: bytes) -> FileOpStatus:
     """Put a file to the user's path or group's path
 
     Args:
-        suffix (str): The email of the user or the id of the group
+        prefix (str): The email of the user or the id of the group
         full_name (str): The path of the file (including file name)
         content (bytes): The content of the file
 
     Returns:
         FileOpStatus: The status of the operation
     """
+    if '@' in prefix:
+        path_prefix = hashlib.md5(prefix.encode()).hexdigest()
+    else:
+        path_prefix = prefix
+    full_path = os.path.join(path_prefix, full_name)
+    if not file.check_path(full_path):
+        return FileOpStatus.PathErr
+    if not file.put_file(full_path, content):
+        return FileOpStatus.Collision
     return FileOpStatus.Ok
 
 
-def create_dir(suffix: str, full_path: str) -> FileOpStatus:
+def create_dir(prefix: str, full_path: str) -> FileOpStatus:
     """Create a directory to the user's path or group's path
 
     Args:
-        suffix (str): The email of the user or the id of the group
+        prefix (str): The email of the user or the id of the group
         full_path (str): The full path of the new directory
 
     Returns:
         FileOpStatus: The status of the operation
     """
+    if '@' in prefix:
+        path_prefix = hashlib.md5(prefix.encode()).hexdigest()
+    else:
+        path_prefix = prefix
+    full_path = os.path.join(path_prefix, full_path)
+    if not file.check_path(full_path):
+        return FileOpStatus.PathErr
+    if not file.create_dir(full_path):
+        return FileOpStatus.Collision
     return FileOpStatus.Ok
 
 
-def del_dir(suffix: str, full_path: str) -> FileOpStatus:
-    """Delete a directory and all files in it to the user's path or group's path
+def del_dir(prefix: str, full_path: str) -> FileOpStatus:
+    """Delete a directory and all files in it (or a single file) to the user's path or group's path
 
     Args:
-        suffix (str): The email of the user or the id of the group
+        prefix (str): The email of the user or the id of the group
         full_path (str): The full path of the directory
 
     Returns:
         FileOpStatus: The status of the operation
     """
-    return FileOpStatus.Ok
-
-
-def del_file(suffix: str, full_name: str) -> FileOpStatus:
-    """Delete a file to the user's path or group's path
-
-    Args:
-        suffix (str): The email of the user or the id of the group
-        full_name (str): The path of the file (including file name)
-
-    Returns:
-        FileOpStatus: The status of the operation
-    """
+    if '@' in prefix:
+        path_prefix = hashlib.md5(prefix.encode()).hexdigest()
+    else:
+        path_prefix = prefix
+    full_path = os.path.join(path_prefix, full_path)
+    if not file.del_dir(full_path):
+        return FileOpStatus.PathErr
     return FileOpStatus.Ok
 
 
@@ -159,4 +245,12 @@ def join_group(email: str, id: str) -> bool:
     Returns:
         bool: True if joined, False otherwise
     """
-    return False
+    if db.get_user(email) is None:
+        return False
+    if not db.check_group(id):
+        file.create_dir(id)
+    return db.add_group_user(id, email)
+
+
+if __name__ == '__main__':
+    gen_authcode('jinyi.xia@bupt.edu.cn')
