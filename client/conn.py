fc7438ca86b9e8d657e0b81b3de820d12c8de0eb
uivnsd <17703035668@163.com>
2023-09-06 04:40:18 +0800

    已实现加密文件加密信道传输，实现创建文件夹，删除文件/文件夹，注册登录

--- client/conn.py
+++ client/conn.py
@@ -1,8 +1,21 @@
-from enum import Enum
-from flask import jsonify
 import socket
+from enum import Enum
+from CA_verify import verify_certificate, extract_public_key_from_certificate
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+from SignatureSystem import verify_sig
+import base64
+import hashlib
+import json
+import os
+from cryptography import x509
+import Crypto.PublicKey.RSA
+import Crypto.Cipher.PKCS1_v1_5
+import Crypto.Random
+import Crypto.Cipher.AES
 
-class ConnStatus:
+
+class ConnStatus(Enum):
     Ok = 0
     Closed = 1
 
@@ -11,81 +24,86 @@ class ServerConn:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            self.sock.connect(('10.21.205.47', 2057))
+            self.sock.connect(('127.0.0.1', 2057))
             self.status = ConnStatus.Ok
         except:
             self.status = ConnStatus.Closed
         self.key = None
 
+        p = 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF
+        g = 2
+        n1 = int(os.urandom(32).hex(), 16)
+        self.sock.send(str(n1).encode())
+        
+        # 接收，n2, g^a, CA(S)的json文件
+        msg = self.sock.recv(4096)
+        msg = json.loads(msg.decode())
+        
+        # 调用验证CA的算法
+        n2 = msg['n2']
+        g_a = msg['g_a']
+        CA = base64.b64decode(msg['CA'])
+        CA = x509.load_pem_x509_certificate(CA, default_backend())
+        ca_public_pem_path = "ca_public_key.pem"
+        with open(ca_public_pem_path, 'rb') as file:
+            ca_public_pem = file.read()
+        ca_public_pem = ca_public_pem
+        ca_public_key = serialization.load_pem_public_key(ca_public_pem, backend=default_backend())
+        sig_public_key = None
+        if verify_certificate(CA, ca_public_key):
+            sig_public_key = extract_public_key_from_certificate(CA)
+        b = int(os.urandom(32).hex(), 16)
+        g_b = pow(g, b, p)
+        self.sock.send(str(g_b).encode())
+
+        # 接收并验证签名算法
+        sig_s = self.sock.recv(4096)
+        verify_sig(sig_s, sig_public_key)
+ 
+        self.key = pow(g_a, b, p)
+        key_bytes = self.key.to_bytes(256, byteorder='big')
+        sha256 = hashlib.sha256()
+        sha256.update(key_bytes)
+        self.key = sha256.digest()
+
     def send(self, msg: bytes) -> bool:
-        cipher_msg = msg
+        iv = Crypto.Random.get_random_bytes(16)
+        aes = Crypto.Cipher.AES.new(self.key, Crypto.Cipher.AES.MODE_CFB, iv)
+        cipher_msg = iv + aes.encrypt(msg)
         try:
             self.sock.send(cipher_msg)
             return True
         except:
             self.status = ConnStatus.Closed
             return False
-
+        
     def recv(self) -> bytes:
-        cipher_msg = b''
         try:
             cipher_msg = self.sock.recv(4096)
-            print(cipher_msg)
         except:
             self.status = ConnStatus.Closed
-            print(" except : ")
             return b''
         self.sock.setblocking(False)
         while True:
             try:
                 data = self.sock.recv(4096)
-                break
             except socket.error as e:
-                print("lld : " ,e.errno)
-                if e.errno == 10035:  # Resource temporarily unavailable
-                    # continue
-                    break
-                else:
-                    break
+                # if e.errno == 10035:  # Resource temporarily unavailable
+                #     continue
+                # else:
+                break
             if not data:
                 break
             cipher_msg += data
         self.sock.setblocking(True)
-        plain_msg = cipher_msg
-        print("plain_msg : ", plain_msg)
+        iv = cipher_msg[:16]
+        aes = Crypto.Cipher.AES.new(self.key, Crypto.Cipher.AES.MODE_CFB, iv)
+        plain_msg = aes.decrypt(cipher_msg[16:])
         return plain_msg
-
+    
     def close(self) -> None:
         self.sock.close()
         self.status = ConnStatus.Closed
 
     def stat(self) -> ConnStatus:
         return self.status
-
-    def recv_file(self, local_path: str) -> bool:
-        with open(local_path, "wb") as f:
-            tmpdata = b' {test} '
-            try:
-                data = self.sock.recv(4096)
-                print(" 11 : ",data)
-                tmpdata = data
-            except:
-                self.status = ConnStatus.Closed
-                return False
-            f.write(tmpdata)
-            self.sock.setblocking(False)
-            while True:
-                try:
-                    data = self.sock.recv(4096)
-                    tmpdata += data
-                    print(" 11 : ",data)
-                except socket.error as e:
-                    # if e.errno == 10035:
-                    #     continue
-                    # else:
-                    break
-                if not data:
-                    break
-                f.write(tmpdata)
-        self.sock.setblocking(True)
-        return True
