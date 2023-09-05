83a4c1507fb2aecb878f7c2fba9635ad9494998e
Jinyi Xia <jinyi.xia@bupt.edu.cn>
2023-09-06 03:24:33 +0800

    Update client_demo.py

--- service/client_demo.py
+++ service/client_demo.py
@@ -1,5 +1,18 @@
 import socket
 from enum import Enum
+from CA_verify import verify_certificate, extract_public_key_from_certificate
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+from SignatureSystem import verify_sig
+import base64
+import hashlib
+import json
+import os
+from cryptography import x509
+import Crypto.PublicKey.RSA
+import Crypto.Cipher.PKCS1_v1_5
+import Crypto.Random
+import Crypto.Cipher.AES
 
 
 class ConnStatus(Enum):
@@ -17,8 +30,46 @@ class ServerConn:
             self.status = ConnStatus.Closed
         self.key = None
 
+        p = 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF
+        g = 2
+        n1 = int(os.urandom(32).hex(), 16)
+        self.sock.send(str(n1).encode())
+        
+        # 接收，n2, g^a, CA(S)的json文件
+        msg = self.sock.recv(4096)
+        msg = json.loads(msg.decode())
+        
+        # 调用验证CA的算法
+        n2 = msg['n2']
+        g_a = msg['g_a']
+        CA = base64.b64decode(msg['CA'])
+        CA = x509.load_pem_x509_certificate(CA, default_backend())
+        ca_public_pem_path = "ca_public_key.pem"
+        with open(ca_public_pem_path, 'rb') as file:
+            ca_public_pem = file.read()
+        ca_public_pem = ca_public_pem
+        ca_public_key = serialization.load_pem_public_key(ca_public_pem, backend=default_backend())
+        sig_public_key = None
+        if verify_certificate(CA, ca_public_key):
+            sig_public_key = extract_public_key_from_certificate(CA)
+        b = int(os.urandom(32).hex(), 16)
+        g_b = pow(g, b, p)
+        self.sock.send(str(g_b).encode())
+
+        # 接收并验证签名算法
+        sig_s = self.sock.recv(4096)
+        verify_sig(sig_s, sig_public_key)
+ 
+        self.key = pow(g_a, b, p)
+        key_bytes = self.key.to_bytes(256, byteorder='big')
+        sha256 = hashlib.sha256()
+        sha256.update(key_bytes)
+        self.key = sha256.digest()
+
     def send(self, msg: bytes) -> bool:
-        cipher_msg = msg
+        iv = Crypto.Random.get_random_bytes(16)
+        aes = Crypto.Cipher.AES.new(self.key, Crypto.Cipher.AES.MODE_CFB, iv)
+        cipher_msg = iv + aes.encrypt(msg)
         try:
             self.sock.send(cipher_msg)
             return True
@@ -37,15 +88,17 @@ class ServerConn:
             try:
                 data = self.sock.recv(4096)
             except socket.error as e:
-                if e.errno == 10035:  # Resource temporarily unavailable
-                    continue
-                else:
-                    break
+                # if e.errno == 10035:  # Resource temporarily unavailable
+                #     continue
+                # else:
+                break
             if not data:
                 break
             cipher_msg += data
         self.sock.setblocking(True)
-        plain_msg = cipher_msg
+        iv = cipher_msg[:16]
+        aes = Crypto.Cipher.AES.new(self.key, Crypto.Cipher.AES.MODE_CFB, iv)
+        plain_msg = aes.decrypt(cipher_msg[16:])
         return plain_msg
     
     def close(self) -> None:
@@ -68,10 +121,10 @@ class ServerConn:
                 try:
                     data = self.sock.recv(4096)
                 except socket.error as e:
-                    if e.errno == 10035:
-                        continue
-                    else:
-                        break
+                    # if e.errno == 10035:
+                    #     continue
+                    # else:
+                    break
                 if not data:
                     break
                 f.write(data)
@@ -80,7 +133,7 @@ class ServerConn:
     
 
 if __name__ == '__main__':
-    import secrets
+    # import secrets
     conn = ServerConn()
     # # generate authcode
     # conn.send(b'{"op": "gen-authcode", "email": "jinyi.xia@bupt.edu.cn"}')
@@ -108,6 +161,6 @@ if __name__ == '__main__':
     conn.send(b'{"op": "get-dir-list", "id": "jinyi.xia@bupt.edu.cn", "path": "."}')
     print(conn.recv())
     # get file
-    conn.send(b'{"op": "get-file", "id": "jinyi.xia@bupt.edu.cn", "path": "./hello.txt"}')
-    conn.recv_file('downloaded.txt')
+    # conn.send(b'{"op": "get-file", "id": "jinyi.xia@bupt.edu.cn", "path": "./hello.txt"}')
+    # conn.recv_file('downloaded.txt')
     conn.close()
