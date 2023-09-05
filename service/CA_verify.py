b02b5e14d1f6b5cbba4f1b32f95541999f8d212c
Jinyi Xia <jinyi.xia@bupt.edu.cn>
2023-09-06 03:25:34 +0800

    Add libraries.

---
+++ service/CA_verify.py
@@ -0,0 +1,55 @@
+import datetime
+
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.x509.oid import NameOID
+
+
+def load_certificate_file(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            certificate = file.read()
+        return certificate
+    except FileNotFoundError:
+        print(f"文件 {file_name} 未找到")
+        return None
+    except IOError:
+        print("文件读取错误")
+        return None
+
+
+def verify_certificate(cert, ca_public_key):
+    try:
+        # 用CA的公钥验证证书
+        ca_public_key.verify(
+            cert.signature,
+            cert.tbs_certificate_bytes,
+            padding.PKCS1v15(),
+            cert.signature_hash_algorithm,
+        )
+        return True
+    except Exception as e:
+        print(f"证书验证失败: {e}")
+        return False
+
+
+def extract_public_key_from_certificate(certificate):
+
+
+    # 提取公钥
+    public_key = certificate.public_key()
+    #这里的公钥是cryptography库中用于表示RSA公钥的内部类型。只是对象的内存地址，而不会显示具体的公钥信息。
+
+    # 打印公钥信息
+    pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+    return pem.decode('utf-8')#解码后得到公钥的具体信息
+
+
+
