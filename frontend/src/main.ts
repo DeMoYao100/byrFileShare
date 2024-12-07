3267b6864a4801515fc1156495928ffe50bc72bb
uivnsd <108260916+uivnsd@users.noreply.github.com>
2024-12-07 17:22:39 +0800

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,6 +7,7 @@ import store from "./store";
 import "@/access";
 import ArcoVueIcon from "@arco-design/web-vue/es/icon";
 
+
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 createApp(App)
