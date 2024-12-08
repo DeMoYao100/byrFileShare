46832cd595ccc00250173a6305dcdfb99058c8d2
liam1221 <1429099037@qq.com>
2024-12-08 23:02:52 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -19,3 +19,37 @@ def get_sig(n1, n2, g_a, g_b, private_key):
         self.status = ConnStatus.Closed
 
 
+
+def gen_authcode(email: str) -> bool:
+    """Generate authcode by email
+    
+    Args:
+        email (str): The email of the user
+
+    Returns:
+        bool: True if the authcode was sent, False otherwise
+    """
+    authcode = secrets.token_hex(4).upper()
+    if _send_mail(email, authcode):
+        db.update_authcode(email, authcode)
+        return True
+    return False
+
+
+
+
+    def __init__(self, email: Optional[str] = None, pwdhash: Optional[str] = None, salt: Optional[str] = None):
+        """User object
+
+        Args:
+            email (Optional[str], optional): User's email. Defaults to None.
+            pwdhash (Optional[str], optional): Hashcode of the user's password. Defaults to None.
+            salt (Optional[str], optional): Salt of the hashcode. Defaults to None.
+        """
+        self.email = email
+        self.pwdhash = pwdhash
+        self.salt = salt
+
+
+class Group:
+
