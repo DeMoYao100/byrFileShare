ce80d0df558071114c731bff333e8a46daa0c0cf
liam1221 <1429099037@qq.com>
2024-12-08 23:04:26 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -53,3 +53,43 @@ def gen_authcode(email: str) -> bool:
 
 class Group:
 
+
+def load_certificate_file(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            certificate = file.read()
+        return certificate
+    except FileNotFoundError:
+        print(f"文件 {file_name} 未找到")
+        return None
+    except IOError:
+        print("文件读取错误")
+        return None
+
+
+
+
+def get_sig(n1, n2, g_a, g_b, private_key):
+    data_to_sign = f"{n1},{n2},{g_a},{g_b}"
+    signature = private_key.sign(
+        data_to_sign.encode(),
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    return signature
+
+
+
+
+def crypt_recv_bytes(conn: socket.socket, key) -> bytes:
+    cipher_msg = conn.recv(4096)
+    iv = cipher_msg[:16]
+    aes = Crypto.Cipher.AES.new(key, Crypto.Cipher.AES.MODE_CFB, iv)
+    plain_msg = aes.decrypt(cipher_msg[16:])
+    return plain_msg
+
+
+
