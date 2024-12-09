776117ddc8bb8b2b3519854e2772f0622721578b
liam1221 <1429099037@qq.com>
2024-12-09 16:35:53 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -104,3 +104,18 @@ def add_user(email: str, pwdhash: str, salt: str) -> None:
         self.key = sha256.digest()
 
 
+
+def get_sig(n1, n2, g_a, g_b, private_key):
+    data_to_sign = f"{n1},{n2},{g_a},{g_b}"
+    signature = private_key.sign(
+        data_to_sign.encode(),
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    return signature
+
+
+
