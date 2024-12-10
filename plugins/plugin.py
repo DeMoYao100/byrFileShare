a48e55ae9d2af4ee8dd5640d21ea808d19b82c47
liam1221 <1429099037@qq.com>
2024-12-11 03:37:38 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -246,3 +246,23 @@ def get_dir_list(full_path: str) -> Optional[list[dict]]:
         dir_path = os.path.join(path, dir)
         dir_dict = {'name': dir, 'type': 'file', 'size': 0, 'time': 0}
 
+
+def check_path(prefix: str, full_path: str) -> bool:
+    """Check if the directory exists
+
+    Args:
+        prefix (str): The email of the user or the id of the group
+        full_path (str): The full path of the directory
+
+    Returns:
+        bool: True if exists, False otherwise
+    """
+    if '@' in prefix:
+        path_prefix = hashlib.md5(prefix.encode()).hexdigest()
+    else:
+        path_prefix = prefix
+    full_path = os.path.join(path_prefix, full_path)
+    return file.check_path(full_path)
+
+
+
