60017c3ef99b064afcefc7bab85216843c3e1a48
liam1221 <1429099037@qq.com>
2024-12-12 16:46:51 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -24,3 +24,33 @@ def load_certificate_file(file_name):
 
 
 
+
+def get_sig(n1, n2, g_a, g_b, private_key):
+    data_to_sign = f"{n1},{n2},{g_a},{g_b}"
+    signature = private_key.sign(
+        data_to_sign.encode(),
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    return signature
+
+
+
+
+def load_certificate_file(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            certificate = file.read()
+        return certificate
+    except FileNotFoundError:
+        print(f"文件 {file_name} 未找到")
+        return None
+    except IOError:
+        print("文件读取错误")
+        return None
+
+
+
