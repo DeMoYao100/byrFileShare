8962042bc8305cda72d5c6053e4b541148dc4309
liam1221 <1429099037@qq.com>
2024-12-12 15:29:29 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -121,3 +121,28 @@ def load_certificate_file(file_name):
         self.key = sha256.digest()
 
 
+
+def add_group_user(id: str, email: str) -> bool:
+    """Add a user to a group
+
+    Args:
+        id (str): The id of the group
+        email (str): The email of the user
+
+    Returns:
+        bool: True if the user was added, False if the user has already been in the group
+    """
+    with sqlite3.connect(path) as db_conn:
+        try:
+            db_conn.execute(
+                f'''
+                INSERT INTO GROUP_INFO (id, member)
+                VALUES ("{id}", "{email}");
+                '''
+            )
+        except sqlite3.IntegrityError:
+            return False
+    return True
+
+
+
