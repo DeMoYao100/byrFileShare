98ab7a38e57a442fc8b738a4b54a0aa82015c24d
liam1221 <1429099037@qq.com>
2024-12-13 19:56:00 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -1,1 +1,24 @@
 # Placeholder
+
+def put_file(prefix: str, full_name: str, content: bytes) -> FileOpStatus:
+    """Put a file to the user's path or group's path
+
+    Args:
+        prefix (str): The email of the user or the id of the group
+        full_name (str): The path of the file (including file name)
+        content (bytes): The content of the file
+
+    Returns:
+        FileOpStatus: The status of the operation
+    """
+    if '@' in prefix:
+        path_prefix = hashlib.md5(prefix.encode()).hexdigest()
+    else:
+        path_prefix = prefix
+    full_path = os.path.join(path_prefix, full_name)
+    if not file.put_file(full_path, content):
+        return FileOpStatus.Collision
+    return FileOpStatus.Ok
+
+
+
