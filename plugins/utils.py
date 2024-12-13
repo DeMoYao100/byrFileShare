3dcc4eb0b0f84a5af365b566f5f8b06344fe0679
liam1221 <1429099037@qq.com>
2024-12-13 21:47:06 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -22,3 +22,25 @@ def put_file(prefix: str, full_name: str, content: bytes) -> FileOpStatus:
 
 
 
+
+def del_dir(prefix: str, full_path: str) -> FileOpStatus:
+    """Delete a directory and all files in it (or a single file) to the user's path or group's path
+
+    Args:
+        prefix (str): The email of the user or the id of the group
+        full_path (str): The full path of the directory
+
+    Returns:
+        FileOpStatus: The status of the operation
+    """
+    if '@' in prefix:
+        path_prefix = hashlib.md5(prefix.encode()).hexdigest()
+    else:
+        path_prefix = prefix
+    full_path = os.path.join(path_prefix, full_path)
+    if not file.del_dir(full_path):
+        return FileOpStatus.PathErr
+    return FileOpStatus.Ok
+
+
+
