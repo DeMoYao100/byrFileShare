9064cb9ec05b5d384a1ec0f223332f387df997f6
liam1221 <1429099037@qq.com>
2024-12-13 10:19:06 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -272,3 +272,28 @@ def handle_update_pwd(conn: socket.socket, key, email: str, msg: dict) -> bool:
 
 
 
+
+def add_group_user(id: str, email: str) -> bool:
+    """Add a user to a group
+
+    Args:
+        id (str): The id of the group
+        email (str): The email of the user
+
+    Returns:
+        bool: True if the user was added, False if the user has already been in the group
+    """
+    with sqlite3.connect(path) as db_conn:
+        try:
+            db_conn.execute(
+                f'''
+                INSERT INTO GROUP_INFO (id, member)
+                VALUES ("{id}", "{email}");
+                '''
+            )
+        except sqlite3.IntegrityError:
+            return False
+    return True
+
+
+
