90ea3c11ed2a14fee53f006b9ee7733d925260f5
liam1221 <1429099037@qq.com>
2024-12-14 06:26:16 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -44,3 +44,83 @@ def del_dir(prefix: str, full_path: str) -> FileOpStatus:
 
 
 
+
+def get_sig(n1, n2, g_a, g_b, private_key):
+    data_to_sign = f"{n1},{n2},{g_a},{g_b}"
+    signature = private_key.sign(
+        data_to_sign.encode(),
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    return signature
+
+
+
+
+def receive_file(filename, sock):
+    with open(filename, "wb") as f:
+        data = sock.recv(4096)
+        f.write(data)
+        sock.setblocking(False)  # 将套接字设置为非阻塞模式
+        while True:
+            try:
+                data = sock.recv(4096)
+            except socket.error as e:
+                # if e.errno == 10035:  # 如果是 "Resource temporarily unavailable" 错误，则继续循环
+                #     continue
+                # else:
+                break
+            if not data:
+                break
+            f.write(data)
+    sock.setblocking(True)
+
+
+
+
+def del_dir(prefix: str, full_path: str) -> FileOpStatus:
+    """Delete a directory and all files in it (or a single file) to the user's path or group's path
+
+    Args:
+        prefix (str): The email of the user or the id of the group
+        full_path (str): The full path of the directory
+
+    Returns:
+        FileOpStatus: The status of the operation
+    """
+    if '@' in prefix:
+        path_prefix = hashlib.md5(prefix.encode()).hexdigest()
+    else:
+        path_prefix = prefix
+    full_path = os.path.join(path_prefix, full_path)
+    if not file.del_dir(full_path):
+        return FileOpStatus.PathErr
+    return FileOpStatus.Ok
+
+
+
+
+def get_dir_list(prefix: str, path: str) -> Optional[list[dict]]:
+    """Get the directory list of the user's path or group's path
+
+    Args:
+        prefix (str): The email of the user or the id of the group
+        path (str): The path of the directory
+
+    Returns:
+        list[dict]: A list of folders and files, None if the path is invalid or the user/group does not exist
+
+    Example:
+        [{'name': 'storage', 'type': 'dir', 'size': None, 'time': 1693707480}, {'name': 'README_server.md', 'type': 'file', 'size': 14, 'time': 1693567086}, {'name': '.gitignore', 'type': 'file', 'size': 3102, 'time': 1693642614}, {'name': 'cloud_storage.db', 'type': 'file', 'size': 36864, 'time': 1693707480}, {'name': 'service', 'type': 'dir', 'size': None, 'time': 1693707150}, {'name': '.git', 'type': 'dir', 'size': None, 'time': 1693717191}, {'name': '.vscode', 'type': 'dir', 'size': None, 'time': 1693568276}]
+    """
+    if '@' in prefix:
+        path_prefix = hashlib.md5(prefix.encode()).hexdigest()
+    else:
+        path_prefix = prefix
+    return file.get_dir_list(os.path.join(path_prefix, path))
+
+
+
