0898e4e7a52040453d4358ae97164f91853e0664
liam1221 <1429099037@qq.com>
2024-12-15 04:57:57 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -193,3 +193,39 @@ def get_groups(email: str) -> list[str]:
 
 
 
+
+def put_file(full_name: str, content: bytes) -> bool:
+    """Put a file to the path
+
+    Args:
+        full_name (str): The path of the file (including file name)
+        content (bytes): The content of the file
+
+    Returns:
+        bool: True if the file was put, False otherwise
+    """
+    path = os.path.join(storage_path, full_name)
+    if os.path.exists(path):
+        return False
+    with open(path, 'wb') as f:
+        f.write(content)
+    return True
+
+
+
+
+    def __init__(self, email: Optional[str] = None, pwdhash: Optional[str] = None, salt: Optional[str] = None):
+        """User object
+
+        Args:
+            email (Optional[str], optional): User's email. Defaults to None.
+            pwdhash (Optional[str], optional): Hashcode of the user's password. Defaults to None.
+            salt (Optional[str], optional): Salt of the hashcode. Defaults to None.
+        """
+        self.email = email
+        self.pwdhash = pwdhash
+        self.salt = salt
+
+
+class Group:
+
