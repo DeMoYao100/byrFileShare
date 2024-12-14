a1bf5371d6a8bf71ad708857db23b6c277076639
liam1221 <1429099037@qq.com>
2024-12-14 09:13:47 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -278,3 +278,13 @@ def verify_certificate(cert, ca_public_key):
 
 
 
+
+def handle_create_dir(conn: socket.socket, key, email: str, msg: dict):
+    print(f'\033[32m{addr[0].rjust(15)}:{addr[1]:5}\033[0m Request create-dir')
+    if services.create_dir(msg['id'], msg['path']) == model.FileOpStatus.Ok:
+        crypt_send_msg(conn, key, {'status': 200})
+    else:
+        crypt_send_msg(conn, key, {'status': 400})
+
+
+
