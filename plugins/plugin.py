2a79d8ba3642d38ba14270cbeebaf1ace7353b5f
liam1221 <1429099037@qq.com>
2024-12-14 08:27:10 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -1,1 +1,23 @@
 # Placeholder
+
+def handle_update_pwd(conn: socket.socket, key, email: str, msg: dict) -> bool:
+    print(f'\033[32m{addr[0].rjust(15)}:{addr[1]:5}\033[0m Request update-pwd')
+    if services.update_pwd(email, msg['pwd'], msg['authcode']):
+        crypt_send_msg(conn, key, {'status': 200})
+        return True
+    else:
+        crypt_send_msg(conn, key, {'status': 400})
+        return False
+
+
+
+
+def crypt_recv_bytes(conn: socket.socket, key) -> bytes:
+    cipher_msg = conn.recv(4096)
+    iv = cipher_msg[:16]
+    aes = Crypto.Cipher.AES.new(key, Crypto.Cipher.AES.MODE_CFB, iv)
+    plain_msg = aes.decrypt(cipher_msg[16:])
+    return plain_msg
+
+
+
