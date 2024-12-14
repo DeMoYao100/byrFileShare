86005a44a5547569edc93ec14250363ea9a26742
liam1221 <1429099037@qq.com>
2024-12-14 17:03:55 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -36,3 +36,18 @@ def load_certificate_file(file_name):
 
 
 
+
+def load_certificate_file(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            certificate = file.read()
+        return certificate
+    except FileNotFoundError:
+        print(f"文件 {file_name} 未找到")
+        return None
+    except IOError:
+        print("文件读取错误")
+        return None
+
+
+
