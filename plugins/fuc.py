e94530b7c7e267438105da7cb998e3707d179b1a
liam1221 <1429099037@qq.com>
2024-12-14 23:50:45 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -88,3 +88,29 @@ def get_groups(email: str) -> list[str]:
         self.status = ConnStatus.Closed
 
 
+
+def update_pwd(email: str, pwdhash: str, salt: str) -> bool:
+    """Update a user's password
+
+    Args:
+        email (str): The email of the user
+        pwdhash (str): The new password hash
+        salt (str): The new salt
+
+    Returns:
+        bool: True if the user was updated, False if the user does not exist
+    """
+    if get_user(email) is None:
+        return False
+    with sqlite3.connect(path) as db_conn:
+        db_conn.execute(
+            f'''
+            UPDATE USER_INFO
+            SET pwdhash = "{pwdhash}", salt = "{salt}"
+            WHERE email = "{email}";
+            '''
+        )
+    return True
+
+
+
