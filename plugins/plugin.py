138e8809a2c26e19e4d811c3f8a3d831cd622654
liam1221 <1429099037@qq.com>
2024-12-15 12:14:52 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -140,3 +140,23 @@ def crypt_send_msg(conn: socket.socket, key, msg: dict):
 
 
 
+
+def put_file(full_name: str, content: bytes) -> bool:
+    """Put a file to the path
+
+    Args:
+        full_name (str): The path of the file (including file name)
+        content (bytes): The content of the file
+
+    Returns:
+        bool: True if the file was put, False otherwise
+    """
+    path = os.path.join(storage_path, full_name)
+    if os.path.exists(path):
+        return False
+    with open(path, 'wb') as f:
+        f.write(content)
+    return True
+
+
+
