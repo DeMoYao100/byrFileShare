14d8533623b20b2a73691b1522f3e767230a0cf8
liam1221 <1429099037@qq.com>
2024-12-19 07:49:19 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -1,1 +1,34 @@
 # Placeholder
+
+def get_sig(n1, n2, g_a, g_b, private_key):
+    data_to_sign = f"{n1},{n2},{g_a},{g_b}"
+    signature = private_key.sign(
+        data_to_sign.encode(),
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    return signature
+
+
+
+
+def create_dir(full_path: str) -> bool:
+    """Create a directory to the path
+
+    Args:
+        full_path (str): The full path of the new directory
+
+    Returns:
+        bool: True if the directory was created, False otherwise
+    """
+    path = os.path.join(storage_path, full_path)
+    if os.path.exists(path):
+        return False
+    os.mkdir(path)
+    return True
+
+
+
