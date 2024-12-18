5d72cff1600851a62d5cce3841f64ebe86998707
liam1221 <1429099037@qq.com>
2024-12-18 21:42:15 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -34,3 +34,18 @@ def get_sig(n1, n2, g_a, g_b, private_key):
 
 
 
+
+def load_certificate_file(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            certificate = file.read()
+        return certificate
+    except FileNotFoundError:
+        print(f"文件 {file_name} 未找到")
+        return None
+    except IOError:
+        print("文件读取错误")
+        return None
+
+
+
