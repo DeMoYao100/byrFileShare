7cbc43394e511a3b98805a5145145d09504d791c
liam1221 <1429099037@qq.com>
2024-12-18 16:36:12 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -1,1 +1,21 @@
 # Placeholder
+
+def recv_long(conn: socket.socket) -> bytes:
+    msg = conn.recv(4096)
+    conn.setblocking(False)
+    while True:
+        try:
+            data = conn.recv(4096)
+        except socket.error as e:
+            # if e.errno == 10035:  # Resource temporarily unavailable
+            #     continue
+            # else:
+            break
+        if not data:
+            break
+        msg += data
+    conn.setblocking(True)
+    return msg
+
+
+
