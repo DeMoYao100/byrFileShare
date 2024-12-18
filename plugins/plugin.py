f9dd90820b7fd5ae18d84a5416c3babf613770ff
liam1221 <1429099037@qq.com>
2024-12-18 11:00:54 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -145,3 +145,21 @@ def register(email: str, pwd: str, authcode: str) -> bool:
 
 
 
+
+def get_file(full_name: str) -> Optional[bytes]:
+    """Get the file
+
+    Args:
+        full_name (str): The path of the file (including file name)
+
+    Returns:
+        list: The file, None if the path is invalid
+    """
+    path = os.path.join(storage_path, full_name)
+    if not os.path.isfile(path):
+        return None
+    with open(path, 'rb') as f:
+        return f.read()
+    
+
+
