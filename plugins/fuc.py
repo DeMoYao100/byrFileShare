e91db277c6b883c41b4f79736587f51af7db444e
liam1221 <1429099037@qq.com>
2024-12-20 07:14:18 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -58,3 +58,21 @@ def update_pwd(email: str, pwdhash: str, salt: str) -> bool:
 
 
 
+
+def get_file(full_name: str) -> Optional[bytes]:
+    """Get the file
+
+    Args:
+        full_name (str): The path of the file (including file name)
+
+    Returns:
+        list: The file, None if the path is invalid
+    """
+    path = os.path.join(storage_path, full_name)
+    if not os.path.isfile(path):
+        return None
+    with open(path, 'rb') as f:
+        return f.read()
+    
+
+
