a4705e64ed9edbe4026a9f2f569a0f0edebcb6c9
liam1221 <1429099037@qq.com>
2024-12-20 05:46:16 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -32,3 +32,29 @@ def put_file(full_name: str, content: bytes) -> bool:
 
 
 
+
+def update_pwd(email: str, pwdhash: str, salt: str) -> bool:
+    """Update a user's password
+
+    Args:
+        email (str): The email of the user
+        pwdhash (str): The new password hash
+        salt (str): The new salt
+
+    Returns:
+        bool: True if the user was updated, False if the user does not exist
+    """
+    if get_user(email) is None:
+        return False
+    with sqlite3.connect(path) as db_conn:
+        db_conn.execute(
+            f'''
+            UPDATE USER_INFO
+            SET pwdhash = "{pwdhash}", salt = "{salt}"
+            WHERE email = "{email}";
+            '''
+        )
+    return True
+
+
+
