9100910bd3a61a5f16afc4530ce077f1f3832680
liam1221 <1429099037@qq.com>
2024-12-19 19:33:32 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -76,3 +76,13 @@ def receive_file(filename, sock):
 
 
 
+
+def handle_del_dir(conn: socket.socket, key, email: str, msg: dict):
+    print(f'\033[32m{addr[0].rjust(15)}:{addr[1]:5}\033[0m Request del-dir')
+    if services.del_dir(msg['id'], msg['path']) == model.FileOpStatus.Ok:
+        crypt_send_msg(conn, key, {'status': 200})
+    else:
+        crypt_send_msg(conn, key, {'status': 400})
+
+
+
