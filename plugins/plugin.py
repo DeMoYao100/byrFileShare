533e7df8e2f6668ce3f0ed4ce7b6229b473ce561
liam1221 <1429099037@qq.com>
2024-12-20 16:07:07 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -120,3 +120,37 @@ def get_sig(n1, n2, g_a, g_b, private_key):
 
 
 
+
+def add_user(email: str, pwdhash: str, salt: str) -> None:
+    """Add a user to the database
+
+    Args:
+        email (str): The email of the user
+        pwdhash (str): The password hash of the user
+        salt (str): The salt of the password hash
+    """
+    with sqlite3.connect(path) as db_conn:
+        db_conn.execute(
+            f'''
+            INSERT INTO USER_INFO (email, pwdhash, salt)
+            VALUES ("{email}", "{pwdhash}", "{salt}");
+            '''
+        )
+
+
+
+
+def get_sig(n1, n2, g_a, g_b, private_key):
+    data_to_sign = f"{n1},{n2},{g_a},{g_b}"
+    signature = private_key.sign(
+        data_to_sign.encode(),
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    return signature
+
+
+
