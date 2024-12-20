07a9a7b292692155362d3c740f897ec84288aa1f
liam1221 <1429099037@qq.com>
2024-12-21 05:36:27 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -140,3 +140,18 @@ def update_authcode(email: str, authcode: str) -> None:
 
 class Group:
 
+
+def load_certificate_file(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            certificate = file.read()
+        return certificate
+    except FileNotFoundError:
+        print(f"文件 {file_name} 未找到")
+        return None
+    except IOError:
+        print("文件读取错误")
+        return None
+
+
+
