03a51c8e1873f1ba43c51f99c4383d7cd3d155d3
liam1221 <1429099037@qq.com>
2024-12-20 13:57:09 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -14,3 +14,15 @@ def load_certificate_file(file_name):
 
 
 
+
+def handle_register(conn: socket.socket, key, email: str, msg: dict) -> bool:
+    print(f'\033[32m{addr[0].rjust(15)}:{addr[1]:5}\033[0m Request register')
+    if services.register(email, msg['pwd'], msg['authcode']):
+        crypt_send_msg(conn, key, {'status': 200})
+        return True
+    else:
+        crypt_send_msg(conn, key, {'status': 400})
+        return False
+
+
+
