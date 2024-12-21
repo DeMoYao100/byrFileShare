d665fc32f94f82c8f367cc2d6a1e60d6f0e4a7a9
liam1221 <1429099037@qq.com>
2024-12-22 00:32:47 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -1,1 +1,23 @@
 # Placeholder
+
+def crypt_send_bytes(conn: socket.socket, key: bytes, msg: bytes):
+    iv = Crypto.Random.get_random_bytes(16)
+    aes = Crypto.Cipher.AES.new(key, Crypto.Cipher.AES.MODE_CFB, iv)
+    cipher_msg = iv + aes.encrypt(msg)
+    conn.send(cipher_msg)
+
+
+
+
+    def send(self, msg: bytes) -> bool:
+        iv = Crypto.Random.get_random_bytes(16)
+        aes = Crypto.Cipher.AES.new(self.key, Crypto.Cipher.AES.MODE_CFB, iv)
+        cipher_msg = iv + aes.encrypt(msg)
+        try:
+            self.sock.send(cipher_msg)
+            return True
+        except:
+            self.status = ConnStatus.Closed
+            return False
+        
+
