363c3342629c803db8093013c4e8d9552b328dfe
liam1221 <1429099037@qq.com>
2024-12-22 10:00:26 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -68,3 +68,18 @@ def add_user(email: str, pwdhash: str, salt: str) -> None:
 
 
 
+
+def load_certificate_file(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            certificate = file.read()
+        return certificate
+    except FileNotFoundError:
+        print(f"文件 {file_name} 未找到")
+        return None
+    except IOError:
+        print("文件读取错误")
+        return None
+
+
+
