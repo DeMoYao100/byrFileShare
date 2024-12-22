c67e2e700039281bdc75132cc73d8e972a3eb50d
liam1221 <1429099037@qq.com>
2024-12-22 23:48:55 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -220,3 +220,22 @@ def handle_put_file(conn: socket.socket, key, email: str, msg: dict):
 
 
 
+
+def get_dir_list(full_path: str) -> Optional[list[dict]]:
+    """Get the directory list
+
+    Args:
+        full_path (str): The path of the directory
+
+    Returns:
+        list: The directory list, None if the path is invalid
+    """
+    path = os.path.join(storage_path, full_path)
+    if not os.path.isdir(path):
+        return None
+    dirs = os.listdir(path)
+    retval = []
+    for dir in dirs:
+        dir_path = os.path.join(path, dir)
+        dir_dict = {'name': dir, 'type': 'file', 'size': 0, 'time': 0}
+
