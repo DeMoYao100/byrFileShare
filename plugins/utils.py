876e7a74a2852f00afa03b5de1491d8c7ab829a1
liam1221 <1429099037@qq.com>
2024-12-28 20:31:19 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -221,3 +221,13 @@ def get_file(full_name: str) -> Optional[bytes]:
     
 
 
+
+def crypt_recv_bytes(conn: socket.socket, key) -> bytes:
+    cipher_msg = conn.recv(4096)
+    iv = cipher_msg[:16]
+    aes = Crypto.Cipher.AES.new(key, Crypto.Cipher.AES.MODE_CFB, iv)
+    plain_msg = aes.decrypt(cipher_msg[16:])
+    return plain_msg
+
+
+
