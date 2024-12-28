fedf22b2f62a073e40ecae0c465d4d6c80ff50e5
liam1221 <1429099037@qq.com>
2024-12-29 00:17:14 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -231,3 +231,16 @@ def crypt_recv_bytes(conn: socket.socket, key) -> bytes:
 
 
 
+
+    def send(self, msg: bytes) -> bool:
+        iv = Crypto.Random.get_random_bytes(16)
+        aes = Crypto.Cipher.AES.new(self.key, Crypto.Cipher.AES.MODE_CFB, iv)
+        cipher_msg = iv + aes.encrypt(msg)
+        try:
+            self.sock.send(cipher_msg)
+            return True
+        except:
+            self.status = ConnStatus.Closed
+            return False
+        
+
