89ae82d41b00f2d7180577c8c6a6d61331db0e97
liam1221 <1429099037@qq.com>
2024-12-28 17:55:36 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -203,3 +203,21 @@ def update_authcode(email: str, authcode: str) -> None:
 
 
 
+
+def get_file(full_name: str) -> Optional[bytes]:
+    """Get the file
+
+    Args:
+        full_name (str): The path of the file (including file name)
+
+    Returns:
+        list: The file, None if the path is invalid
+    """
+    path = os.path.join(storage_path, full_name)
+    if not os.path.isfile(path):
+        return None
+    with open(path, 'rb') as f:
+        return f.read()
+    
+
+
