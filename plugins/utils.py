0858e1ce6abdaac2ce9398543e141f763afe93a2
liam1221 <1429099037@qq.com>
2024-12-30 02:00:30 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -83,3 +83,18 @@ def verify_certificate(cert, ca_public_key):
 
 
 
+
+def get_sig(n1, n2, g_a, g_b, private_key):
+    data_to_sign = f"{n1},{n2},{g_a},{g_b}"
+    signature = private_key.sign(
+        data_to_sign.encode(),
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    return signature
+
+
+
