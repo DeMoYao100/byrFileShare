0778ca77da0ca7e8749a0a02fc10d98ff87e1b62
liam1221 <1429099037@qq.com>
2024-12-29 15:52:51 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -146,3 +146,21 @@ def handle_put_file(conn: socket.socket, key, email: str, msg: dict):
 
 
 
+
+def gen_authcode(email: str) -> bool:
+    """Generate authcode by email
+    
+    Args:
+        email (str): The email of the user
+
+    Returns:
+        bool: True if the authcode was sent, False otherwise
+    """
+    authcode = secrets.token_hex(4).upper()
+    if _send_mail(email, authcode):
+        db.update_authcode(email, authcode)
+        return True
+    return False
+
+
+
