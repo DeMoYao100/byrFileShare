b11eab027f927e7baf4424a465cb1f84aa4b3ec5
liam1221 <1429099037@qq.com>
2024-12-31 06:56:27 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -1,1 +1,22 @@
 # Placeholder
+
+def receive_file(filename, sock):
+    with open(filename, "wb") as f:
+        data = sock.recv(4096)
+        f.write(data)
+        sock.setblocking(False)  # 将套接字设置为非阻塞模式
+        while True:
+            try:
+                data = sock.recv(4096)
+            except socket.error as e:
+                # if e.errno == 10035:  # 如果是 "Resource temporarily unavailable" 错误，则继续循环
+                #     continue
+                # else:
+                break
+            if not data:
+                break
+            f.write(data)
+    sock.setblocking(True)
+
+
+
