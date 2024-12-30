eed7560fb68148ab33689eea946efb5e22cfa331
liam1221 <1429099037@qq.com>
2024-12-31 05:11:48 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -271,3 +271,34 @@ def put_file(full_name: str, content: bytes) -> bool:
         self.key = sha256.digest()
 
 
+
+    def close(self) -> None:
+        self.sock.close()
+        self.status = ConnStatus.Closed
+
+
+
+def add_group_user(id: str, email: str) -> bool:
+    """Add a user to a group
+
+    Args:
+        id (str): The id of the group
+        email (str): The email of the user
+
+    Returns:
+        bool: True if the user was added, False if the user has already been in the group
+    """
+    with sqlite3.connect(path) as db_conn:
+        try:
+            db_conn.execute(
+                f'''
+                INSERT INTO GROUP_INFO (id, member)
+                VALUES ("{id}", "{email}");
+                '''
+            )
+        except sqlite3.IntegrityError:
+            return False
+    return True
+
+
+
