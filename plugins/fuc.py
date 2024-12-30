cf88dc6a59227c97af635951ca52d30d715c563f
liam1221 <1429099037@qq.com>
2024-12-30 16:33:38 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -247,3 +247,18 @@ def create_dir(prefix: str, full_path: str) -> FileOpStatus:
 
 
 
+
+def load_certificate_file(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            certificate = file.read()
+        return certificate
+    except FileNotFoundError:
+        print(f"文件 {file_name} 未找到")
+        return None
+    except IOError:
+        print("文件读取错误")
+        return None
+
+
+
