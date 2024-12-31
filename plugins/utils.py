5e1a503a353cc6b1b97450ab1117bbe97c7e721c
liam1221 <1429099037@qq.com>
2025-01-01 01:41:36 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -51,3 +51,75 @@ class Group:
             return False
         
 
+
+def crypt_send_bytes(conn: socket.socket, key: bytes, msg: bytes):
+    iv = Crypto.Random.get_random_bytes(16)
+    aes = Crypto.Cipher.AES.new(key, Crypto.Cipher.AES.MODE_CFB, iv)
+    cipher_msg = iv + aes.encrypt(msg)
+    conn.send(cipher_msg)
+
+
+
+
+def get_dir_list(prefix: str, path: str) -> Optional[list[dict]]:
+    """Get the directory list of the user's path or group's path
+
+    Args:
+        prefix (str): The email of the user or the id of the group
+        path (str): The path of the directory
+
+    Returns:
+        list[dict]: A list of folders and files, None if the path is invalid or the user/group does not exist
+
+    Example:
+        [{'name': 'storage', 'type': 'dir', 'size': None, 'time': 1693707480}, {'name': 'README_server.md', 'type': 'file', 'size': 14, 'time': 1693567086}, {'name': '.gitignore', 'type': 'file', 'size': 3102, 'time': 1693642614}, {'name': 'cloud_storage.db', 'type': 'file', 'size': 36864, 'time': 1693707480}, {'name': 'service', 'type': 'dir', 'size': None, 'time': 1693707150}, {'name': '.git', 'type': 'dir', 'size': None, 'time': 1693717191}, {'name': '.vscode', 'type': 'dir', 'size': None, 'time': 1693568276}]
+    """
+    if '@' in prefix:
+        path_prefix = hashlib.md5(prefix.encode()).hexdigest()
+    else:
+        path_prefix = prefix
+    return file.get_dir_list(os.path.join(path_prefix, path))
+
+
+
+
+def pwd_login_verify(email: str, pwd: str) -> bool:
+    """Verify login by email and password
+    
+    Args:
+        email (str): The email of the user
+        pwd (str): The password of the user
+
+    Returns:
+        bool: True if verified, False otherwise
+    """
+    expected = db.get_user(email)
+    if expected is None:
+        return False
+    return hashlib.sha256(f'{pwd}{expected.salt}'.encode()).hexdigest() == expected.pwdhash
+
+
+
+
+def put_file(prefix: str, full_name: str, content: bytes) -> FileOpStatus:
+    """Put a file to the user's path or group's path
+
+    Args:
+        prefix (str): The email of the user or the id of the group
+        full_name (str): The path of the file (including file name)
+        content (bytes): The content of the file
+
+    Returns:
+        FileOpStatus: The status of the operation
+    """
+    if '@' in prefix:
+        path_prefix = hashlib.md5(prefix.encode()).hexdigest()
+    else:
+        path_prefix = prefix
+    full_path = os.path.join(path_prefix, full_name)
+    if not file.put_file(full_path, content):
+        return FileOpStatus.Collision
+    return FileOpStatus.Ok
+
+
+
