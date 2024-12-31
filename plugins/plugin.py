bf5be7d02eab6b25a01b8fcc5832393f72bd267d
liam1221 <1429099037@qq.com>
2025-01-01 04:41:28 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -77,3 +77,79 @@ class Group:
         self.status = ConnStatus.Closed
 
 
+
+def verify_certificate(cert, ca_public_key):
+    try:
+        # 用CA的公钥验证证书
+        ca_public_key.verify(
+            cert.signature,
+            cert.tbs_certificate_bytes,
+            padding.PKCS1v15(),
+            cert.signature_hash_algorithm,
+        )
+        return True
+    except Exception as e:
+        print(f"证书验证失败: {e}")
+        return False
+
+
+
+
+def get_file(full_name: str) -> Optional[bytes]:
+    """Get the file
+
+    Args:
+        full_name (str): The path of the file (including file name)
+
+    Returns:
+        list: The file, None if the path is invalid
+    """
+    path = os.path.join(storage_path, full_name)
+    if not os.path.isfile(path):
+        return None
+    with open(path, 'rb') as f:
+        return f.read()
+    
+
+
+
+def update_pwd(email: str, pwdhash: str, salt: str) -> bool:
+    """Update a user's password
+
+    Args:
+        email (str): The email of the user
+        pwdhash (str): The new password hash
+        salt (str): The new salt
+
+    Returns:
+        bool: True if the user was updated, False if the user does not exist
+    """
+    if get_user(email) is None:
+        return False
+    with sqlite3.connect(path) as db_conn:
+        db_conn.execute(
+            f'''
+            UPDATE USER_INFO
+            SET pwdhash = "{pwdhash}", salt = "{salt}"
+            WHERE email = "{email}";
+            '''
+        )
+    return True
+
+
+
+
+def get_sig(n1, n2, g_a, g_b, private_key):
+    data_to_sign = f"{n1},{n2},{g_a},{g_b}"
+    signature = private_key.sign(
+        data_to_sign.encode(),
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    return signature
+
+
+
