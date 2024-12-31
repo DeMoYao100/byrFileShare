460a899eaa224767c982c50efa497686188da3c4
liam1221 <1429099037@qq.com>
2024-12-31 13:40:38 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -1,1 +1,36 @@
 # Placeholder
+
+def handle_join_group(conn: socket.socket, key, email: str, msg: dict):
+    print(f'\033[32m{addr[0].rjust(15)}:{addr[1]:5}\033[0m Request join-group')
+    if services.join_group(email, msg['id']):
+        crypt_send_msg(conn, key, {'status': 200})
+    else:
+        crypt_send_msg(conn, key, {'status': 400})
+
+
+
+
+def create_dir(full_path: str) -> bool:
+    """Create a directory to the path
+
+    Args:
+        full_path (str): The full path of the new directory
+
+    Returns:
+        bool: True if the directory was created, False otherwise
+    """
+    path = os.path.join(storage_path, full_path)
+    if os.path.exists(path):
+        return False
+    os.mkdir(path)
+    return True
+
+
+
+
+def crypt_recv_msg(conn: socket.socket, key) -> dict:
+    plain_msg = crypt_recv_bytes(conn, key)
+    return json.loads(plain_msg.decode())
+
+
+
