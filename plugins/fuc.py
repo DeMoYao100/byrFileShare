18c076a18bbe83dde2debe02c62f2efb4552c13b
liam1221 <1429099037@qq.com>
2025-01-01 10:02:11 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -168,3 +168,23 @@ def get_sig(n1, n2, g_a, g_b, private_key):
         self.status = ConnStatus.Closed
 
 
+
+def check_path(prefix: str, full_path: str) -> bool:
+    """Check if the directory exists
+
+    Args:
+        prefix (str): The email of the user or the id of the group
+        full_path (str): The full path of the directory
+
+    Returns:
+        bool: True if exists, False otherwise
+    """
+    if '@' in prefix:
+        path_prefix = hashlib.md5(prefix.encode()).hexdigest()
+    else:
+        path_prefix = prefix
+    full_path = os.path.join(path_prefix, full_path)
+    return file.check_path(full_path)
+
+
+
