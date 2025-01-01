8b83ddbc004585cab5ff4722b1cef4dab04bab52
liam1221 <1429099037@qq.com>
2025-01-02 03:45:05 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -72,3 +72,21 @@ def get_groups(email: str) -> list[str]:
 
 
 
+
+def create_dir(full_path: str) -> bool:
+    """Create a directory to the path
+
+    Args:
+        full_path (str): The full path of the new directory
+
+    Returns:
+        bool: True if the directory was created, False otherwise
+    """
+    path = os.path.join(storage_path, full_path)
+    if os.path.exists(path):
+        return False
+    os.mkdir(path)
+    return True
+
+
+
