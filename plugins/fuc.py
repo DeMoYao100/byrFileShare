6a37cf84d572ed61947728442438e50ea16d0110
liam1221 <1429099037@qq.com>
2025-01-02 21:45:10 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -237,3 +237,25 @@ def check_group(id: str) -> bool:
 
 class Group:
 
+
+def del_dir(prefix: str, full_path: str) -> FileOpStatus:
+    """Delete a directory and all files in it (or a single file) to the user's path or group's path
+
+    Args:
+        prefix (str): The email of the user or the id of the group
+        full_path (str): The full path of the directory
+
+    Returns:
+        FileOpStatus: The status of the operation
+    """
+    if '@' in prefix:
+        path_prefix = hashlib.md5(prefix.encode()).hexdigest()
+    else:
+        path_prefix = prefix
+    full_path = os.path.join(path_prefix, full_path)
+    if not file.del_dir(full_path):
+        return FileOpStatus.PathErr
+    return FileOpStatus.Ok
+
+
+
