74c3064a0c71a4efb33030a35dfda9b048953a50
liam1221 <1429099037@qq.com>
2025-01-02 19:42:46 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -1,26 +1,5 @@
 # Placeholder
 
-def receive_file(filename, sock):
-    with open(filename, "wb") as f:
-        data = sock.recv(4096)
-        f.write(data)
-        sock.setblocking(False)  # 将套接字设置为非阻塞模式
-        while True:
-            try:
-                data = sock.recv(4096)
-            except socket.error as e:
-                # if e.errno == 10035:  # 如果是 "Resource temporarily unavailable" 错误，则继续循环
-                #     continue
-                # else:
-                break
-            if not data:
-                break
-            f.write(data)
-    sock.setblocking(True)
-
-
-
-
 def get_file(prefix: str, full_name: str) -> Optional[bytes]:
     """Get the file list of the user's path or group's path
 
@@ -40,105 +19,6 @@ def get_file(prefix: str, full_name: str) -> Optional[bytes]:
 
 
 
-    def __init__(self, email: Optional[str] = None, pwdhash: Optional[str] = None, salt: Optional[str] = None):
-        """User object
-
-        Args:
-            email (Optional[str], optional): User's email. Defaults to None.
-            pwdhash (Optional[str], optional): Hashcode of the user's password. Defaults to None.
-            salt (Optional[str], optional): Salt of the hashcode. Defaults to None.
-        """
-        self.email = email
-        self.pwdhash = pwdhash
-        self.salt = salt
-
-
-class Group:
-
-
-    def __init__(self, email: Optional[str] = None, pwdhash: Optional[str] = None, salt: Optional[str] = None):
-        """User object
-
-        Args:
-            email (Optional[str], optional): User's email. Defaults to None.
-            pwdhash (Optional[str], optional): Hashcode of the user's password. Defaults to None.
-            salt (Optional[str], optional): Salt of the hashcode. Defaults to None.
-        """
-        self.email = email
-        self.pwdhash = pwdhash
-        self.salt = salt
-
-
-class Group:
-
-
-    def close(self) -> None:
-        self.sock.close()
-        self.status = ConnStatus.Closed
-
-
-
-def verify_certificate(cert, ca_public_key):
-    try:
-        # 用CA的公钥验证证书
-        ca_public_key.verify(
-            cert.signature,
-            cert.tbs_certificate_bytes,
-            padding.PKCS1v15(),
-            cert.signature_hash_algorithm,
-        )
-        return True
-    except Exception as e:
-        print(f"证书验证失败: {e}")
-        return False
-
-
-
-
-def get_file(full_name: str) -> Optional[bytes]:
-    """Get the file
-
-    Args:
-        full_name (str): The path of the file (including file name)
-
-    Returns:
-        list: The file, None if the path is invalid
-    """
-    path = os.path.join(storage_path, full_name)
-    if not os.path.isfile(path):
-        return None
-    with open(path, 'rb') as f:
-        return f.read()
-    
-
-
-
-def update_pwd(email: str, pwdhash: str, salt: str) -> bool:
-    """Update a user's password
-
-    Args:
-        email (str): The email of the user
-        pwdhash (str): The new password hash
-        salt (str): The new salt
-
-    Returns:
-        bool: True if the user was updated, False if the user does not exist
-    """
-    if get_user(email) is None:
-        return False
-    with sqlite3.connect(path) as db_conn:
-        db_conn.execute(
-            f'''
-            UPDATE USER_INFO
-            SET pwdhash = "{pwdhash}", salt = "{salt}"
-            WHERE email = "{email}";
-            '''
-        )
-    return True
-
-
-
-
 def get_sig(n1, n2, g_a, g_b, private_key):
     data_to_sign = f"{n1},{n2},{g_a},{g_b}"
     signature = private_key.sign(
@@ -154,155 +34,52 @@ def get_sig(n1, n2, g_a, g_b, private_key):
 
 
 
-    def recv(self) -> bytes:
-        try:
-            cipher_msg = self.sock.recv(4096)
-        except:
-            self.status = ConnStatus.Closed
-            return b''
-        self.sock.setblocking(False)
-        while True:
-            try:
-                data = self.sock.recv(4096)
-            except socket.error as e:
-                # if e.errno == 10035:  # Resource temporarily unavailable
-                #     continue
-                # else:
-                break
-            if not data:
-                break
-            cipher_msg += data
-        self.sock.setblocking(True)
-        iv = cipher_msg[:16]
-        aes = Crypto.Cipher.AES.new(self.key, Crypto.Cipher.AES.MODE_CFB, iv)
-        plain_msg = aes.decrypt(cipher_msg[16:])
-        return plain_msg
-    
-
-
-    def __init__(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            self.sock.connect(('127.0.0.1', 2057))
-            self.status = ConnStatus.Ok
-        except:
-            self.status = ConnStatus.Closed
-        self.key = None
-
-        p = 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF
-        g = 2
-        n1 = int(os.urandom(32).hex(), 16)
-        self.sock.send(str(n1).encode())
-        
-        # 接收，n2, g^a, CA(S)的json文件
-        msg = self.sock.recv(4096)
-        msg = json.loads(msg.decode())
-        
-        # 调用验证CA的算法
-        n2 = msg['n2']
-        g_a = msg['g_a']
-        CA = base64.b64decode(msg['CA'])
-        CA = x509.load_pem_x509_certificate(CA, default_backend())
-        ca_public_pem_path = "ca_public_key.pem"
-        with open(ca_public_pem_path, 'rb') as file:
-            ca_public_pem = file.read()
-        ca_public_pem = ca_public_pem
-        ca_public_key = serialization.load_pem_public_key(ca_public_pem, backend=default_backend())
-        pem_public = None
-        if verify_certificate(CA, ca_public_key):
-            pem_public = extract_public_key_from_certificate(CA)
-        sig_public_key = serialization.load_pem_public_key(pem_public, backend=default_backend())
-        b = int(os.urandom(32).hex(), 16)
-        g_b = pow(g, b, p)
-        self.sock.send(str(g_b).encode())
-
-        # 接收并验证签名算法
-        sig_s = self.sock.recv(4096)
-        verify_sig(sig_s, n1, n2, g_a, g_b, sig_public_key)
- 
-        self.key = pow(g_a, b, p)
-        key_bytes = self.key.to_bytes(256, byteorder='big')
-        sha256 = hashlib.sha256()
-        sha256.update(key_bytes)
-        self.key = sha256.digest()
-
-
-
-    def __init__(self, email: Optional[str] = None, pwdhash: Optional[str] = None, salt: Optional[str] = None):
-        """User object
-
-        Args:
-            email (Optional[str], optional): User's email. Defaults to None.
-            pwdhash (Optional[str], optional): Hashcode of the user's password. Defaults to None.
-            salt (Optional[str], optional): Salt of the hashcode. Defaults to None.
-        """
-        self.email = email
-        self.pwdhash = pwdhash
-        self.salt = salt
-
-
-class Group:
-
-
-def verify_certificate(cert, ca_public_key):
-    try:
-        # 用CA的公钥验证证书
-        ca_public_key.verify(
-            cert.signature,
-            cert.tbs_certificate_bytes,
-            padding.PKCS1v15(),
-            cert.signature_hash_algorithm,
-        )
-        return True
-    except Exception as e:
-        print(f"证书验证失败: {e}")
-        return False
-
-
-
-
-def put_file(full_name: str, content: bytes) -> bool:
-    """Put a file to the path
-
-    Args:
-        full_name (str): The path of the file (including file name)
-        content (bytes): The content of the file
-
-    Returns:
-        bool: True if the file was put, False otherwise
-    """
-    path = os.path.join(storage_path, full_name)
+def init(db_path: str = path) -> None:
+    """Initialize the sqlite database"""
+    path = db_path
     if os.path.exists(path):
-        return False
-    with open(path, 'wb') as f:
-        f.write(content)
-    return True
+        with sqlite3.connect(path) as db_conn:
+            db_conn.execute('DELETE FROM AUTHCODE_INFO;')
+        return
+    with sqlite3.connect(path) as db_conn:
+        db_conn.execute(
+            '''
+            CREATE TABLE USER_INFO (
+                email TEXT PRIMARY KEY,
+                pwdhash TEXT NOT NULL,
+                salt TEXT NOT NULL
+            );
+            '''
+        )
+        db_conn.execute(
+            '''
+            CREATE UNIQUE INDEX USER_email ON USER_INFO (email);
+            '''
+        )
+        db_conn.execute(
+            '''
+            CREATE TABLE GROUP_INFO (
+                id TEXT NOT NULL,
+                member TEXT NOT NULL,
+                PRIMARY KEY (id, member),
+                FOREIGN KEY (member) REFERENCES USER_INFO (email) ON DELETE CASCADE
+            );
+            '''
+        )
+        db_conn.execute(
+            '''
+            CREATE INDEX GROUP_id ON GROUP_INFO (member);
+            '''
+        )
+        db_conn.execute(
+            '''
+            CREATE TABLE AUTHCODE_INFO (
+                email TEXT PRIMARY KEY,
+                authcode TEXT NOT NULL,
+                timestamp INTEGER NOT NULL
+            );
+            '''
+        )
 
 
-
-
-    def recv(self) -> bytes:
-        try:
-            cipher_msg = self.sock.recv(4096)
-        except:
-            self.status = ConnStatus.Closed
-            return b''
-        self.sock.setblocking(False)
-        while True:
-            try:
-                data = self.sock.recv(4096)
-            except socket.error as e:
-                # if e.errno == 10035:  # Resource temporarily unavailable
-                #     continue
-                # else:
-                break
-            if not data:
-                break
-            cipher_msg += data
-        self.sock.setblocking(True)
-        iv = cipher_msg[:16]
-        aes = Crypto.Cipher.AES.new(self.key, Crypto.Cipher.AES.MODE_CFB, iv)
-        plain_msg = aes.decrypt(cipher_msg[16:])
-        return plain_msg
-    
 
