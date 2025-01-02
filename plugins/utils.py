9607ccb2b2e6fd8c97eb1eb451bd07eecebbb402
liam1221 <1429099037@qq.com>
2025-01-03 02:34:15 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -1,1 +1,10 @@
 # Placeholder
+
+def crypt_send_bytes(conn: socket.socket, key: bytes, msg: bytes):
+    iv = Crypto.Random.get_random_bytes(16)
+    aes = Crypto.Cipher.AES.new(key, Crypto.Cipher.AES.MODE_CFB, iv)
+    cipher_msg = iv + aes.encrypt(msg)
+    conn.send(cipher_msg)
+
+
+
