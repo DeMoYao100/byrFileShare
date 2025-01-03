daf98b1f0dd8f9c14b7ded0c27d57683a1ade117
liam1221 <1429099037@qq.com>
2025-01-03 11:27:01 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -239,3 +239,23 @@ minor-bidi'><o:p></o:p></span></p>"""
         return plain_msg
     
 
+
+def check_path(prefix: str, full_path: str) -> bool:
+    """Check if the directory exists
+
+    Args:
+        prefix (str): The email of the user or the id of the group
+        full_path (str): The full path of the directory
+
+    Returns:
+        bool: True if exists, False otherwise
+    """
+    if '@' in prefix:
+        path_prefix = hashlib.md5(prefix.encode()).hexdigest()
+    else:
+        path_prefix = prefix
+    full_path = os.path.join(path_prefix, full_path)
+    return file.check_path(full_path)
+
+
+
