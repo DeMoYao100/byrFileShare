070eca43b09ea33be1f714e0e3690a85b940ef04
liam1221 <1429099037@qq.com>
2025-01-03 20:29:51 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -50,3 +50,59 @@ def update_pwd(email: str, pwdhash: str, salt: str) -> bool:
 
 
 
+
+def register(email: str, pwd: str, authcode: str) -> bool:
+    """Register a new user
+    
+    Args:
+        email (str): The email of the user
+        pwd (str): The password of the user
+        authcode (str): The authcode of the user
+
+    Returns:
+        bool: True if registered, False otherwise
+    """
+    if db.get_user(email) is not None or authcode != db.get_authcode(email):
+        return False
+    salt = secrets.token_urlsafe(16)
+    pwdhash = hashlib.sha256(f'{pwd}{salt}'.encode()).hexdigest()
+    db.add_user(email, pwdhash, salt)
+    user_folder = hashlib.md5(email.encode()).hexdigest()
+    file.create_dir(user_folder)
+    return True
+
+
+
+
+def create_dir(full_path: str) -> bool:
+    """Create a directory to the path
+
+    Args:
+        full_path (str): The full path of the new directory
+
+    Returns:
+        bool: True if the directory was created, False otherwise
+    """
+    path = os.path.join(storage_path, full_path)
+    if os.path.exists(path):
+        return False
+    os.mkdir(path)
+    return True
+
+
+
+
+def load_certificate_file(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            certificate = file.read()
+        return certificate
+    except FileNotFoundError:
+        print(f"文件 {file_name} 未找到")
+        return None
+    except IOError:
+        print("文件读取错误")
+        return None
+
+
+
