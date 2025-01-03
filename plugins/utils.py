fc19284d183f47e4f28a0e0b2da5bf8866890371
liam1221 <1429099037@qq.com>
2025-01-03 21:35:28 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -141,3 +141,26 @@ def verify_certificate(cert, ca_public_key):
 
 
 
+
+def register(email: str, pwd: str, authcode: str) -> bool:
+    """Register a new user
+    
+    Args:
+        email (str): The email of the user
+        pwd (str): The password of the user
+        authcode (str): The authcode of the user
+
+    Returns:
+        bool: True if registered, False otherwise
+    """
+    if db.get_user(email) is not None or authcode != db.get_authcode(email):
+        return False
+    salt = secrets.token_urlsafe(16)
+    pwdhash = hashlib.sha256(f'{pwd}{salt}'.encode()).hexdigest()
+    db.add_user(email, pwdhash, salt)
+    user_folder = hashlib.md5(email.encode()).hexdigest()
+    file.create_dir(user_folder)
+    return True
+
+
+
