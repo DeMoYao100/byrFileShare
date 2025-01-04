b7b1a1735e8981db18f64ce79e7fd9a2bdd5a777
liam1221 <1429099037@qq.com>
2025-01-05 05:33:55 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -129,3 +129,68 @@ def add_group_user(id: str, email: str) -> bool:
 
 
 
+
+def get_sig(n1, n2, g_a, g_b, private_key):
+    data_to_sign = f"{n1},{n2},{g_a},{g_b}"
+    signature = private_key.sign(
+        data_to_sign.encode(),
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    return signature
+
+
+
+
+def init(db_path: str = path) -> None:
+    """Initialize the sqlite database"""
+    path = db_path
+    if os.path.exists(path):
+        with sqlite3.connect(path) as db_conn:
+            db_conn.execute('DELETE FROM AUTHCODE_INFO;')
+        return
+    with sqlite3.connect(path) as db_conn:
+        db_conn.execute(
+            '''
+            CREATE TABLE USER_INFO (
+                email TEXT PRIMARY KEY,
+                pwdhash TEXT NOT NULL,
+                salt TEXT NOT NULL
+            );
+            '''
+        )
+        db_conn.execute(
+            '''
+            CREATE UNIQUE INDEX USER_email ON USER_INFO (email);
+            '''
+        )
+        db_conn.execute(
+            '''
+            CREATE TABLE GROUP_INFO (
+                id TEXT NOT NULL,
+                member TEXT NOT NULL,
+                PRIMARY KEY (id, member),
+                FOREIGN KEY (member) REFERENCES USER_INFO (email) ON DELETE CASCADE
+            );
+            '''
+        )
+        db_conn.execute(
+            '''
+            CREATE INDEX GROUP_id ON GROUP_INFO (member);
+            '''
+        )
+        db_conn.execute(
+            '''
+            CREATE TABLE AUTHCODE_INFO (
+                email TEXT PRIMARY KEY,
+                authcode TEXT NOT NULL,
+                timestamp INTEGER NOT NULL
+            );
+            '''
+        )
+
+
+
