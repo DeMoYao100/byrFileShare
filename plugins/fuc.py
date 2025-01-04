68279422a47656adcb723eb3222627499e8e50e1
liam1221 <1429099037@qq.com>
2025-01-04 21:53:09 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -219,3 +219,15 @@ def update_pwd(email: str, pwd: str, authcode: str) -> bool:
 
 
 
+
+def handle_update_pwd(conn: socket.socket, key, email: str, msg: dict) -> bool:
+    print(f'\033[32m{addr[0].rjust(15)}:{addr[1]:5}\033[0m Request update-pwd')
+    if services.update_pwd(email, msg['pwd'], msg['authcode']):
+        crypt_send_msg(conn, key, {'status': 200})
+        return True
+    else:
+        crypt_send_msg(conn, key, {'status': 400})
+        return False
+
+
+
