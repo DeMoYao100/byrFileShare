bab2c0c45e909e89affd37b9007d0ceff3f16f84
liam1221 <1429099037@qq.com>
2025-01-05 07:54:41 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -92,3 +92,97 @@ def verify_certificate(cert, ca_public_key):
 
 
 
+
+    def recv(self) -> bytes:
+        try:
+            cipher_msg = self.sock.recv(4096)
+        except:
+            self.status = ConnStatus.Closed
+            return b''
+        self.sock.setblocking(False)
+        while True:
+            try:
+                data = self.sock.recv(4096)
+            except socket.error as e:
+                # if e.errno == 10035:  # Resource temporarily unavailable
+                #     continue
+                # else:
+                break
+            if not data:
+                break
+            cipher_msg += data
+        self.sock.setblocking(True)
+        iv = cipher_msg[:16]
+        aes = Crypto.Cipher.AES.new(self.key, Crypto.Cipher.AES.MODE_CFB, iv)
+        plain_msg = aes.decrypt(cipher_msg[16:])
+        return plain_msg
+    
+
+
+def get_authcode(email: str) -> Optional[str]:
+    """Get authcode by email
+
+    Args:
+        email (str): The email
+
+    Returns:
+        Optional[str]: The authcode if available, None otherwise
+    """
+    with sqlite3.connect(path) as db_conn:
+        cursor = db_conn.execute(
+            f'''
+            SELECT *
+            FROM AUTHCODE_INFO
+            WHERE email = "{email}"
+            '''
+        )
+        all = cursor.fetchall()
+    if len(all) == 0:
+        return None
+    if all[0][2] + 600 < int(time.time()):
+        del_authcode(email)
+        return None
+    return all[0][1]
+
+
+
+
+def check_group(id: str) -> bool:
+    """Check if a group exists
+
+    Args:
+        id (str): The id of the group
+
+    Returns:
+        bool: True if the group exists, False otherwise
+    """
+    with sqlite3.connect(path) as db_conn:
+        cursor = db_conn.execute(
+            f'''
+            SELECT *
+            FROM GROUP_INFO
+            WHERE id = "{id}"
+            '''
+        )
+        all = cursor.fetchall()
+    return len(all) > 0
+
+
+
+
+def verify_certificate(cert, ca_public_key):
+    try:
+        # 用CA的公钥验证证书
+        ca_public_key.verify(
+            cert.signature,
+            cert.tbs_certificate_bytes,
+            padding.PKCS1v15(),
+            cert.signature_hash_algorithm,
+        )
+        return True
+    except Exception as e:
+        print(f"证书验证失败: {e}")
+        return False
+
+
+
