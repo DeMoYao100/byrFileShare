cdc2f791bb96eb31955181436ad28501c6a772a9
liam1221 <1429099037@qq.com>
2025-01-05 19:37:23 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -1,1 +1,44 @@
 # Placeholder
+
+def check_group(id: str) -> bool:
+    """Check if a group exists
+
+    Args:
+        id (str): The id of the group
+
+    Returns:
+        bool: True if the group exists, False otherwise
+    """
+    with sqlite3.connect(path) as db_conn:
+        cursor = db_conn.execute(
+            f'''
+            SELECT *
+            FROM GROUP_INFO
+            WHERE id = "{id}"
+            '''
+        )
+        all = cursor.fetchall()
+    return len(all) > 0
+
+
+
+
+def update_authcode(email: str, authcode: str) -> None:
+    """Update authcode by email
+
+    Args:
+        email (str): The email
+        authcode (str): The authcode
+    """
+    with sqlite3.connect(path) as db_conn:
+        db_conn.execute(
+            f'''
+            INSERT INTO AUTHCODE_INFO (email, authcode, timestamp)
+            VALUES ("{email}", "{authcode}", {int(time.time())})
+            ON CONFLICT(email) DO UPDATE
+            SET authcode = "{authcode}", timestamp = {int(time.time())};
+            '''
+        )
+
+
+
