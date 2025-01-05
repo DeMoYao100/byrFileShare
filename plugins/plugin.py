f3616756360a6554f0e69b8fedf21f3e29638dcc
liam1221 <1429099037@qq.com>
2025-01-06 07:44:50 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -127,3 +127,16 @@ def pwd_login_verify(email: str, pwd: str) -> bool:
 
 
 
+
+    def send(self, msg: bytes) -> bool:
+        iv = Crypto.Random.get_random_bytes(16)
+        aes = Crypto.Cipher.AES.new(self.key, Crypto.Cipher.AES.MODE_CFB, iv)
+        cipher_msg = iv + aes.encrypt(msg)
+        try:
+            self.sock.send(cipher_msg)
+            return True
+        except:
+            self.status = ConnStatus.Closed
+            return False
+        
+
