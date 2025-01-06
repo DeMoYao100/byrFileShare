54ef6ae2b1aa5ba5733e2966570ab38f213ba4f0
liam1221 <1429099037@qq.com>
2025-01-06 09:55:28 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -140,3 +140,18 @@ def pwd_login_verify(email: str, pwd: str) -> bool:
             return False
         
 
+
+def get_sig(n1, n2, g_a, g_b, private_key):
+    data_to_sign = f"{n1},{n2},{g_a},{g_b}"
+    signature = private_key.sign(
+        data_to_sign.encode(),
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    return signature
+
+
+
