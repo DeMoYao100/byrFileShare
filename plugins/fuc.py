61844cbb2f57da89b1fe539880e6e4a63b2a4cfe
liam1221 <1429099037@qq.com>
2025-01-08 22:16:24 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -204,3 +204,54 @@ def handle_gen_authcode(conn: socket.socket, key, email: str, msg: dict):
 
 
 
+
+def crypt_send_bytes(conn: socket.socket, key: bytes, msg: bytes):
+    iv = Crypto.Random.get_random_bytes(16)
+    aes = Crypto.Cipher.AES.new(key, Crypto.Cipher.AES.MODE_CFB, iv)
+    cipher_msg = iv + aes.encrypt(msg)
+    conn.send(cipher_msg)
+
+
+
+
+def update_authcode(email: str, authcode: str) -> None:
+    """Update authcode by email
+
+    Args:
+        email (str): The email
+        authcode (str): The authcode
+    """
+    with sqlite3.connect(path) as db_conn:
+        db_conn.execute(
+            f'''
+            INSERT INTO AUTHCODE_INFO (email, authcode, timestamp)
+            VALUES ("{email}", "{authcode}", {int(time.time())})
+            ON CONFLICT(email) DO UPDATE
+            SET authcode = "{authcode}", timestamp = {int(time.time())};
+            '''
+        )
+
+
+
+
+def get_dir_list(prefix: str, path: str) -> Optional[list[dict]]:
+    """Get the directory list of the user's path or group's path
+
+    Args:
+        prefix (str): The email of the user or the id of the group
+        path (str): The path of the directory
+
+    Returns:
+        list[dict]: A list of folders and files, None if the path is invalid or the user/group does not exist
+
+    Example:
+        [{'name': 'storage', 'type': 'dir', 'size': None, 'time': 1693707480}, {'name': 'README_server.md', 'type': 'file', 'size': 14, 'time': 1693567086}, {'name': '.gitignore', 'type': 'file', 'size': 3102, 'time': 1693642614}, {'name': 'cloud_storage.db', 'type': 'file', 'size': 36864, 'time': 1693707480}, {'name': 'service', 'type': 'dir', 'size': None, 'time': 1693707150}, {'name': '.git', 'type': 'dir', 'size': None, 'time': 1693717191}, {'name': '.vscode', 'type': 'dir', 'size': None, 'time': 1693568276}]
+    """
+    if '@' in prefix:
+        path_prefix = hashlib.md5(prefix.encode()).hexdigest()
+    else:
+        path_prefix = prefix
+    return file.get_dir_list(os.path.join(path_prefix, path))
+
+
+
