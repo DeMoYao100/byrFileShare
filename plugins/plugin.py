cce2e1a448d1a4f3bd64f6148367a3e8242adcd8
liam1221 <1429099037@qq.com>
2025-02-27 01:45:40 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -257,3 +257,53 @@ def del_dir(prefix: str, full_path: str) -> FileOpStatus:
 
 
 
+
+def init(db_path: str = path) -> None:
+    """Initialize the sqlite database"""
+    path = db_path
+    if os.path.exists(path):
+        with sqlite3.connect(path) as db_conn:
+            db_conn.execute('DELETE FROM AUTHCODE_INFO;')
+        return
+    with sqlite3.connect(path) as db_conn:
+        db_conn.execute(
+            '''
+            CREATE TABLE USER_INFO (
+                email TEXT PRIMARY KEY,
+                pwdhash TEXT NOT NULL,
+                salt TEXT NOT NULL
+            );
+            '''
+        )
+        db_conn.execute(
+            '''
+            CREATE UNIQUE INDEX USER_email ON USER_INFO (email);
+            '''
+        )
+        db_conn.execute(
+            '''
+            CREATE TABLE GROUP_INFO (
+                id TEXT NOT NULL,
+                member TEXT NOT NULL,
+                PRIMARY KEY (id, member),
+                FOREIGN KEY (member) REFERENCES USER_INFO (email) ON DELETE CASCADE
+            );
+            '''
+        )
+        db_conn.execute(
+            '''
+            CREATE INDEX GROUP_id ON GROUP_INFO (member);
+            '''
+        )
+        db_conn.execute(
+            '''
+            CREATE TABLE AUTHCODE_INFO (
+                email TEXT PRIMARY KEY,
+                authcode TEXT NOT NULL,
+                timestamp INTEGER NOT NULL
+            );
+            '''
+        )
+
+
+
