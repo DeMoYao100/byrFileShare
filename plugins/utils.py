8aa215bddbd1bf8b98c88b45d7fec1790b952df4
liam1221 <1429099037@qq.com>
2025-02-27 03:22:10 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -33,3 +33,58 @@ def get_dir_list(full_path: str) -> Optional[list[dict]]:
         dir_path = os.path.join(path, dir)
         dir_dict = {'name': dir, 'type': 'file', 'size': 0, 'time': 0}
 
+
+def get_sig(n1, n2, g_a, g_b, private_key):
+    data_to_sign = f"{n1},{n2},{g_a},{g_b}"
+    signature = private_key.sign(
+        data_to_sign.encode(),
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    return signature
+
+
+
+
+def verify_certificate(cert, ca_public_key):
+    try:
+        # 用CA的公钥验证证书
+        ca_public_key.verify(
+            cert.signature,
+            cert.tbs_certificate_bytes,
+            padding.PKCS1v15(),
+            cert.signature_hash_algorithm,
+        )
+        return True
+    except Exception as e:
+        print(f"证书验证失败: {e}")
+        return False
+
+
+
+
+def register(email: str, pwd: str, authcode: str) -> bool:
+    """Register a new user
+    
+    Args:
+        email (str): The email of the user
+        pwd (str): The password of the user
+        authcode (str): The authcode of the user
+
+    Returns:
+        bool: True if registered, False otherwise
+    """
+    if db.get_user(email) is not None or authcode != db.get_authcode(email):
+        return False
+    salt = secrets.token_urlsafe(16)
+    pwdhash = hashlib.sha256(f'{pwd}{salt}'.encode()).hexdigest()
+    db.add_user(email, pwdhash, salt)
+    user_folder = hashlib.md5(email.encode()).hexdigest()
+    file.create_dir(user_folder)
+    return True
+
+
+
