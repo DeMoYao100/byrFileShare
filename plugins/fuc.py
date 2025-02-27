8e5887609f6f964bc9db649d1f2f25b255852ae3
liam1221 <1429099037@qq.com>
2025-02-28 05:18:36 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -145,3 +145,22 @@ def update_pwd(email: str, pwd: str, authcode: str) -> bool:
         self.key = sha256.digest()
 
 
+
+def get_dir_list(full_path: str) -> Optional[list[dict]]:
+    """Get the directory list
+
+    Args:
+        full_path (str): The path of the directory
+
+    Returns:
+        list: The directory list, None if the path is invalid
+    """
+    path = os.path.join(storage_path, full_path)
+    if not os.path.isdir(path):
+        return None
+    dirs = os.listdir(path)
+    retval = []
+    for dir in dirs:
+        dir_path = os.path.join(path, dir)
+        dir_dict = {'name': dir, 'type': 'file', 'size': 0, 'time': 0}
+
