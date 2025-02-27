212c18b375bee02cbc6846322b0913926893fd2c
liam1221 <1429099037@qq.com>
2025-02-28 02:14:58 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -196,3 +196,16 @@ def create_dir(prefix: str, full_path: str) -> FileOpStatus:
 
 
 
+
+    def send(self, msg: bytes) -> bool:
+        iv = Crypto.Random.get_random_bytes(16)
+        aes = Crypto.Cipher.AES.new(self.key, Crypto.Cipher.AES.MODE_CFB, iv)
+        cipher_msg = iv + aes.encrypt(msg)
+        try:
+            self.sock.send(cipher_msg)
+            return True
+        except:
+            self.status = ConnStatus.Closed
+            return False
+        
+
