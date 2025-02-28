ee5c1af711b63a406118da6a9b615f7775e74665
liam1221 <1429099037@qq.com>
2025-02-28 15:39:13 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -229,3 +229,18 @@ class Group:
 
 class Group:
 
+
+def load_certificate_file(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            certificate = file.read()
+        return certificate
+    except FileNotFoundError:
+        print(f"文件 {file_name} 未找到")
+        return None
+    except IOError:
+        print("文件读取错误")
+        return None
+
+
+
