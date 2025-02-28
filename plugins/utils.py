4d3ba37a7464f56c50e4f2bc56ff7ead8602a8ba
liam1221 <1429099037@qq.com>
2025-02-28 22:47:07 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -12,3 +12,31 @@
             return False
         
 
+
+    def send(self, msg: bytes) -> bool:
+        iv = Crypto.Random.get_random_bytes(16)
+        aes = Crypto.Cipher.AES.new(self.key, Crypto.Cipher.AES.MODE_CFB, iv)
+        cipher_msg = iv + aes.encrypt(msg)
+        try:
+            self.sock.send(cipher_msg)
+            return True
+        except:
+            self.status = ConnStatus.Closed
+            return False
+        
+
+
+def get_sig(n1, n2, g_a, g_b, private_key):
+    data_to_sign = f"{n1},{n2},{g_a},{g_b}"
+    signature = private_key.sign(
+        data_to_sign.encode(),
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    return signature
+
+
+
