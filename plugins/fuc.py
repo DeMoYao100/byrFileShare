3d69cafb1ad9c83022236430dd6ab03949bb82c3
liam1221 <1429099037@qq.com>
2025-02-28 18:07:04 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -244,3 +244,59 @@ def load_certificate_file(file_name):
 
 
 
+
+def verify_certificate(cert, ca_public_key):
+    try:
+        # 用CA的公钥验证证书
+        ca_public_key.verify(
+            cert.signature,
+            cert.tbs_certificate_bytes,
+            padding.PKCS1v15(),
+            cert.signature_hash_algorithm,
+        )
+        return True
+    except Exception as e:
+        print(f"证书验证失败: {e}")
+        return False
+
+
+
+
+def check_group(id: str) -> bool:
+    """Check if a group exists
+
+    Args:
+        id (str): The id of the group
+
+    Returns:
+        bool: True if the group exists, False otherwise
+    """
+    with sqlite3.connect(path) as db_conn:
+        cursor = db_conn.execute(
+            f'''
+            SELECT *
+            FROM GROUP_INFO
+            WHERE id = "{id}"
+            '''
+        )
+        all = cursor.fetchall()
+    return len(all) > 0
+
+
+
+
+    def __init__(self, email: Optional[str] = None, pwdhash: Optional[str] = None, salt: Optional[str] = None):
+        """User object
+
+        Args:
+            email (Optional[str], optional): User's email. Defaults to None.
+            pwdhash (Optional[str], optional): Hashcode of the user's password. Defaults to None.
+            salt (Optional[str], optional): Salt of the hashcode. Defaults to None.
+        """
+        self.email = email
+        self.pwdhash = pwdhash
+        self.salt = salt
+
+
+class Group:
+
