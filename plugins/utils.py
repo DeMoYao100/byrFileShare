0a932e3cc8cd1d588693b59912b1c3e83d6e12cf
liam1221 <1429099037@qq.com>
2025-03-01 17:30:19 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -34,3 +34,70 @@ def get_sig(n1, n2, g_a, g_b, private_key):
 
 
 
+
+def handle_put_file(conn: socket.socket, key, email: str, msg: dict):
+    print(f'\033[32m{addr[0].rjust(15)}:{addr[1]:5}\033[0m Request put-file')
+    if services.check_path(msg['id'], msg['path']):
+        crypt_send_bytes(conn, key, b'400')
+        return
+    else:
+        crypt_send_bytes(conn, key, b'200')
+    if crypt_recv_bytes(conn, key) == b'200':
+        crypt_send_bytes(conn, key, b'200')
+    else:
+        crypt_send_bytes(conn, key, b'400')
+        return
+    fifo_path = f'./tmp/{int(time.time())}.pipe'
+    receive_file(fifo_path, conn)
+    with open(fifo_path, 'rb') as f:
+        cipher_msg = f.read()
+    iv = cipher_msg[:16]
+    aes = Crypto.Cipher.AES.new(key, Crypto.Cipher.AES.MODE_CFB, iv)
+    plain_msg = aes.decrypt(cipher_msg[16:])
+    services.put_file(msg['id'], msg['path'], plain_msg)
+    crypt_send_msg(conn, key, {'status': 200})
+
+
+
+
+def check_group(id: str) -> bool:
+    """Check if a group exists
+
+    Args:
+        id (str): The id of the group
+
+    Returns:
+        bool: True if the group exists, False otherwise
+    """
+    with sqlite3.connect(path) as db_conn:
+        cursor = db_conn.execute(
+            f'''
+            SELECT *
+            FROM GROUP_INFO
+            WHERE id = "{id}"
+            '''
+        )
+        all = cursor.fetchall()
+    return len(all) > 0
+
+
+
+
+def get_dir_list(full_path: str) -> Optional[list[dict]]:
+    """Get the directory list
+
+    Args:
+        full_path (str): The path of the directory
+
+    Returns:
+        list: The directory list, None if the path is invalid
+    """
+    path = os.path.join(storage_path, full_path)
+    if not os.path.isdir(path):
+        return None
+    dirs = os.listdir(path)
+    retval = []
+    for dir in dirs:
+        dir_path = os.path.join(path, dir)
+        dir_dict = {'name': dir, 'type': 'file', 'size': 0, 'time': 0}
+
