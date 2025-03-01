dbbbbb2206ab6cda17c3b1e5b354bfae4abc46e1
liam1221 <1429099037@qq.com>
2025-03-02 03:45:42 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -196,3 +196,23 @@ def crypt_recv_bytes(conn: socket.socket, key) -> bytes:
 
 
 
+
+def recv_long(conn: socket.socket) -> bytes:
+    msg = conn.recv(4096)
+    conn.setblocking(False)
+    while True:
+        try:
+            data = conn.recv(4096)
+        except socket.error as e:
+            # if e.errno == 10035:  # Resource temporarily unavailable
+            #     continue
+            # else:
+            break
+        if not data:
+            break
+        msg += data
+    conn.setblocking(True)
+    return msg
+
+
+
