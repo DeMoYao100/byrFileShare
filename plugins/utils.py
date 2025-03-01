0ea4e6ac30b033c054ca14484bd65a257f8a1f1d
liam1221 <1429099037@qq.com>
2025-03-01 19:11:17 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -150,3 +150,21 @@ def load_certificate_file(file_name):
 
 
 
+
+def create_dir(full_path: str) -> bool:
+    """Create a directory to the path
+
+    Args:
+        full_path (str): The full path of the new directory
+
+    Returns:
+        bool: True if the directory was created, False otherwise
+    """
+    path = os.path.join(storage_path, full_path)
+    if os.path.exists(path):
+        return False
+    os.mkdir(path)
+    return True
+
+
+
