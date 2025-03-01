ea4dafd046d5cc1d161369d7d15a978ca5f6fcf5
liam1221 <1429099037@qq.com>
2025-03-02 00:53:28 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -188,3 +188,28 @@ def recv_long(conn: socket.socket) -> bytes:
 
 
 
+
+def add_group_user(id: str, email: str) -> bool:
+    """Add a user to a group
+
+    Args:
+        id (str): The id of the group
+        email (str): The email of the user
+
+    Returns:
+        bool: True if the user was added, False if the user has already been in the group
+    """
+    with sqlite3.connect(path) as db_conn:
+        try:
+            db_conn.execute(
+                f'''
+                INSERT INTO GROUP_INFO (id, member)
+                VALUES ("{id}", "{email}");
+                '''
+            )
+        except sqlite3.IntegrityError:
+            return False
+    return True
+
+
+
