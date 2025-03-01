aaa5ed9a9417f8a1e0a3417f6e985a168c3392e7
liam1221 <1429099037@qq.com>
2025-03-01 09:24:37 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -268,3 +268,51 @@ def put_file(prefix: str, full_name: str, content: bytes) -> FileOpStatus:
         self.key = sha256.digest()
 
 
+
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.sock.connect(('127.0.0.1', 2057))
+            self.status = ConnStatus.Ok
+        except:
+            self.status = ConnStatus.Closed
+        self.key = None
+
+        p = 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF
+        g = 2
+        n1 = int(os.urandom(32).hex(), 16)
+        self.sock.send(str(n1).encode())
+        
+        # 接收，n2, g^a, CA(S)的json文件
+        msg = self.sock.recv(4096)
+        msg = json.loads(msg.decode())
+        
+        # 调用验证CA的算法
+        n2 = msg['n2']
+        g_a = msg['g_a']
+        CA = base64.b64decode(msg['CA'])
+        CA = x509.load_pem_x509_certificate(CA, default_backend())
+        ca_public_pem_path = "ca_public_key.pem"
+        with open(ca_public_pem_path, 'rb') as file:
+            ca_public_pem = file.read()
+        ca_public_pem = ca_public_pem
+        ca_public_key = serialization.load_pem_public_key(ca_public_pem, backend=default_backend())
+        pem_public = None
+        if verify_certificate(CA, ca_public_key):
+            pem_public = extract_public_key_from_certificate(CA)
+        sig_public_key = serialization.load_pem_public_key(pem_public, backend=default_backend())
+        b = int(os.urandom(32).hex(), 16)
+        g_b = pow(g, b, p)
+        self.sock.send(str(g_b).encode())
+
+        # 接收并验证签名算法
+        sig_s = self.sock.recv(4096)
+        verify_sig(sig_s, n1, n2, g_a, g_b, sig_public_key)
+ 
+        self.key = pow(g_a, b, p)
+        key_bytes = self.key.to_bytes(256, byteorder='big')
+        sha256 = hashlib.sha256()
+        sha256.update(key_bytes)
+        self.key = sha256.digest()
+
+
