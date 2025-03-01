3b7db02d76b3f6a7fe317420bab548a3b22b6ff6
liam1221 <1429099037@qq.com>
2025-03-01 21:28:58 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -121,3 +121,21 @@ def handle_register(conn: socket.socket, key, email: str, msg: dict) -> bool:
 
 
 
+
+def get_file(full_name: str) -> Optional[bytes]:
+    """Get the file
+
+    Args:
+        full_name (str): The path of the file (including file name)
+
+    Returns:
+        list: The file, None if the path is invalid
+    """
+    path = os.path.join(storage_path, full_name)
+    if not os.path.isfile(path):
+        return None
+    with open(path, 'rb') as f:
+        return f.read()
+    
+
+
