645a88060cc9add2ed2a38bb734076a25966e996
liam1221 <1429099037@qq.com>
2025-03-01 22:51:22 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -168,3 +168,23 @@ def create_dir(full_path: str) -> bool:
 
 
 
+
+def recv_long(conn: socket.socket) -> bytes:
+    msg = conn.recv(4096)
+    conn.setblocking(False)
+    while True:
+        try:
+            data = conn.recv(4096)
+        except socket.error as e:
+            # if e.errno == 10035:  # Resource temporarily unavailable
+            #     continue
+            # else:
+            break
+        if not data:
+            break
+        msg += data
+    conn.setblocking(True)
+    return msg
+
+
+
