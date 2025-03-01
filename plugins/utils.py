4f0c5d452da3d1de2c0ad8b58783ecb65ccdd95c
liam1221 <1429099037@qq.com>
2025-03-01 18:26:02 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -117,3 +117,21 @@ def get_dir_list(full_path: str) -> Optional[list[dict]]:
 
 class Group:
 
+
+def get_file(full_name: str) -> Optional[bytes]:
+    """Get the file
+
+    Args:
+        full_name (str): The path of the file (including file name)
+
+    Returns:
+        list: The file, None if the path is invalid
+    """
+    path = os.path.join(storage_path, full_name)
+    if not os.path.isfile(path):
+        return None
+    with open(path, 'rb') as f:
+        return f.read()
+    
+
+
