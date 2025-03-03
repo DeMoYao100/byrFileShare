42225518df3580da421ec7a8e4c1acae9d1acccd
liam1221 <1429099037@qq.com>
2025-03-03 18:57:33 +0800

    Automated modification of utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -5,3 +5,45 @@
         self.status = ConnStatus.Closed
 
 
+
+def get_dir_list(full_path: str) -> Optional[list[dict]]:
+    """Get the directory list
+
+    Args:
+        full_path (str): The path of the directory
+
+    Returns:
+        list: The directory list, None if the path is invalid
+    """
+    path = os.path.join(storage_path, full_path)
+    if not os.path.isdir(path):
+        return None
+    dirs = os.listdir(path)
+    retval = []
+    for dir in dirs:
+        dir_path = os.path.join(path, dir)
+        dir_dict = {'name': dir, 'type': 'file', 'size': 0, 'time': 0}
+
+
+def get_groups(email: str) -> list[str]:
+    """Get a list of groups ids that a user is a member of
+
+    Args:
+        email (str): The email of the user
+
+    Returns:
+        list[str]: A list of group ids
+    """
+    with sqlite3.connect(path) as db_conn:
+        cursor = db_conn.execute(
+            f'''
+            SELECT id
+            FROM GROUP_INFO
+            WHERE member = "{email}"
+            '''
+        )
+        all = cursor.fetchall()
+    return [g[0] for g in all]
+
+
+
