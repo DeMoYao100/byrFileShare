b2ca10da7aaf55cd24071beb6bbcacb7c12b1302
liam1221 <1429099037@qq.com>
2025-03-03 20:35:13 +0800

    Automated modification of fuc.py

--- plugins/fuc.py
+++ plugins/fuc.py
@@ -116,3 +116,18 @@ def handle_update_pwd(conn: socket.socket, key, email: str, msg: dict) -> bool:
 
 
 
+
+def load_certificate_file(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            certificate = file.read()
+        return certificate
+    except FileNotFoundError:
+        print(f"文件 {file_name} 未找到")
+        return None
+    except IOError:
+        print("文件读取错误")
+        return None
+
+
+
