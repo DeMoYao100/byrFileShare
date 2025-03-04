dff5395147c0c28d97873d6523d65375aa54d983
liam1221 <1429099037@qq.com>
2025-03-04 15:24:33 +0800

    Automated modification of plugin.py

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -41,3 +41,21 @@ class Group:
         return plain_msg
     
 
+
+def gen_authcode(email: str) -> bool:
+    """Generate authcode by email
+    
+    Args:
+        email (str): The email of the user
+
+    Returns:
+        bool: True if the authcode was sent, False otherwise
+    """
+    authcode = secrets.token_hex(4).upper()
+    if _send_mail(email, authcode):
+        db.update_authcode(email, authcode)
+        return True
+    return False
+
+
+
